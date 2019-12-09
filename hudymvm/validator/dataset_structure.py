1d944966cb5195146f924ed816af2bea70eedaad
asya <qwerty.com>
2019-12-09 15:14:26 +0200

    upload

---
+++ hudymvm/validator/dataset_structure.py
@@ -0,0 +1,23 @@
+dataset = {
+     'AVpe__eOilAPnD_xSt-H':
+              {
+        'id': 'AVpe__eOilAPnD_xSt-H',
+        'categories':'Access.,Clothing,Shoes,Women\'s Clothing',
+        'keys':'handcraftedalpacablendpurplecharismasweaterperu/19238764,novica/231516,handcraftedalpacablendpurplecharismasweaterperubynovica/19238764',
+        'name':'Handcrafted Alpaca Blend \'Purple Charisma\' Sweater (Peru)'
+              },
+     'AVpe_19KilAPnD_xSqnY':
+          {
+        'id':'AVpe_19KilAPnD_xSqnY',
+        'categories':'Shoes,Clothing,Women\'s Shoes,All Women\'s Shoes',
+        'keys':'0656487173401,caparros/h1705blkslv,656487173401',
+        'name':'Caparros Karissa Open Toe Canvas Sandals'
+         },
+     'AVpe_0ob1cnluZ0-bnWr':
+              {
+        'id':'AVpe_0ob1cnluZ0-bnWr',
+        'categories':'Shoes,Women\'s Shoes,Clothing,All Women\'s Shoes',
+        'keys':'096506461056,0096506461056,sofcomfortwomenseverydaygelinsoles2pair/552675364,beautifeet/46105',
+        'name':'Sof Comfort Women\'s Everyday Gel Insoles, 2-pair'
+        }
+          }
