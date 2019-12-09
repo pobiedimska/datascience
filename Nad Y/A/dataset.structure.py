45d6752da8d539fb02c7115aea53b453add49263
Yuliya-Nad <nad.yul2002@gmail.com>
2019-12-09 15:27:17 +0200

    dataset made

---
+++ Nad Y/A/dataset.structure.py
@@ -0,0 +1,24 @@
+#color - string
+#brand - string
+#review - string
+#weight - float
+Data = {
+    "shoe1" : {
+        "color" : "Black",
+        "brand" : "Journee Collection",
+        "review" : None,
+        "weight": None
+        },
+    "shoe2" : {
+    "color": "Brown",
+    "brand": "Ralph Lauren",
+    "review": "Solid",
+    "weight": None
+    },
+    "shoe3" : {
+        "color": "Green",
+        "brand": "Skechers",
+        "review": None,
+        "weight": None
+    }
+}
