84ab23839d0b442537f2a76fee609522188972fd
KateandEva <pavlovskakate2002@gmail.com>
2019-12-09 14:49:05 +0200

    dataset_structure and description

---
+++ oliinykao/dataset_structure.py
@@ -0,0 +1,20 @@
+dataset = {
+                "1 пара": {
+                            "id": "AVpe__eOilAPnD_xSt-H",
+                            "brand": "Novica",
+                            "colors": "Purple",
+                            "flavors": ""
+                          },
+                "2 пара": {
+                            "id": "AVpe_0ob1cnluZ0-bnWr",
+                            "brand": "BEAUTIFEET",
+                            "colors": "Multicolor,MULTI",
+                            "flavors": ""
+                          },
+                "3 пара": {
+                            "id": "AVpe_19KilAPnD_xSqnY",
+                            "brand": "Caparros",
+                            "colors": "Silver",
+                            "flavors": ""
+                          }
+            }
