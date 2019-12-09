738c517d6bf0d02d5ee242d9b345f7ba740fc1e5
Your Name <you@example.com>
2019-12-09 15:24:10 +0200

    pull request dataset_structure

---
+++ DrobyazkoOV/dataset_structure.py
@@ -0,0 +1,17 @@
+dataset = {
+        "AVpe__eOilAPnD_xSt-H": {
+            "name": "Handcrafted1",
+            "prices.offer": {46, 567, 453},
+            "size": {41, 42, 43, 44}
+        },
+        "AVpe__eOilAPnD_xSt": {
+            "name": "Handcrafted2",
+            "prices.offer": {478, 57, 4531},
+            "size": {41, 42, 44}
+        },
+        "AVpe__eOilAPnD_t-H": {
+            "name": "Handcrafted3",
+            "prices.offer": {46, 567, 453},
+            "size": {41, 42, 43, 44}
+        },
+}
