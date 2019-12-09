db07909cc445a02db3978d943ba8a94c01269ba8
dmytrob <krolegyb@gmail.com>
2019-12-09 15:13:30 +0200

    Added stuff

---
+++ burdenkods/dataset_structure.py
@@ -0,0 +1,25 @@
+# keys - string
+# asins - UUID ?
+# name - string
+# size - string
+
+ShoeData = {
+    "ID0000001": {
+        "keys": "amazon.com/listing/123456",
+        "asins": None,
+        "name": "Vintage Louboutin, worn",
+        "size": "8"
+    },
+    "ID0000002": {
+        "keys": "amazon.com/listing/123488876",
+        "asins": None,
+        "name": "Authentic Tory Burch",
+        "size": "7.5"
+    },
+    "ID0000003": {
+        "keys": "walmart.com/listing/123456",
+        "asins": None,
+        "name": "Basic Chelsea Boot",
+        "size": "7.5"
+    }
+}
