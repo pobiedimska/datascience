4d6d74ee48cb690efd412cd1ccbe9f0bc8f710eb
Your Name <you@example.com>
2019-12-09 15:10:34 +0200

    my code

---
+++ PoliatskaAA/dataset_structure.py
@@ -0,0 +1,16 @@
+datasaet = {
+            "AVpe__e0ilAPnD_xSt-H": {
+                                     "color": "black",
+                                     "ean": "980982193"
+                                      },
+
+            "AVpe__hvgkFGk_jk": {
+                                    "color": "purple",
+                                    "ean": "981112193"
+                                    },
+
+            "VHGk_gu-hiud": {
+                                "color": "black",
+                                "ean": "4000982193"
+                             }
+                }
