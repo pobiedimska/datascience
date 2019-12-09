6af963809fe91fd88b5e313a26829317f2eb5bd4
Your Name <you@example.com>
2019-12-09 15:08:57 +0200

    task1

---
+++ miroshnyk_a_v/validator/dataset_structure.py
@@ -0,0 +1,33 @@
+dataset={
+    '1':{'name':{'brend':"",
+                 'type':"",
+                 'country':''},
+         'size':123,
+         'quantities':99,
+         'reviews':'none'},
+    '2':{'name':{'brend':"",
+                 'type':"",
+                 'country':''},
+         'size':23,
+         'quantities':98,
+         'reviews':'none'},
+    '3':{'name':{'brend':"",
+                 'type':"",
+                 'country':''},
+         'size':13,
+         'quantities':9,
+         'reviews':'none'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
