9048e599281d8bf7e9c3551ff7feb4c33ed43d57
EugeneBudzinskiy <eugene.unusual@gmail.com>
2019-12-09 15:10:33 +0200

    Create DataSet and description

---
+++ budzinskiyeo/dataset_structure.py
@@ -0,0 +1,43 @@
+data = {
+    "AVpe__JXilAPnD_xSt3F": {
+        "flavors": 000002,
+        "count": 000001,
+        "categories": {
+            "Shoes": None,
+            "Clothing": None,
+            "Women's Shoes": None,
+            "All Women's Shoes": None
+        },
+        "brand": {
+            "Ralph Lauren": None
+        }
+    },
+
+    "AVpe_0mrilAPnD_xSqI7": {
+        "flavors": 001002,
+        "count": 000201,
+        "categories": {
+            "Shoes": None,
+            "Clothing": None,
+            "Women's Shoes": None,
+            "All Women's Shoes": None
+        },
+        "brand": {
+            "Ralph Lauren": None
+        }
+    },
+
+    "AVpe_0ob1cnluZ0-bnWr": {
+        "flavors": 001302,
+        "count": 000211,
+        "categories": {
+            "Shoes": None,
+            "Clothing": None,
+            "Women's Shoes": None,
+            "All Women's Shoes": None
+        },
+        "brand": {
+            "Ralph Lauren": None
+        }
+    }
+}
