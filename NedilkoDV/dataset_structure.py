93d91fe5135a6ffbd4527969461d3fd51d687b8b
Your Name <you@example.com>
2019-12-09 15:23:15 +0200

    dataset_structure

---
+++ NedilkoDV/dataset_structure.py
@@ -0,0 +1,19 @@
+dataset={'AVpe__eOilAPnD_xSt-H1':{
+                                'count': null,
+                                'key': 'handmade',
+                                'brand':'Novica'},
+
+        'AVpe__eOilAPnD_xSt-H2':{
+                                'count': null,
+                                'key': 'handmade',
+                                'brand':'Novica'},
+
+
+        'AVpe__eOilAPnD_xSt-H3':{
+                                'count': null,
+                                'key': 'handmade',
+                                'brand':'Novica'
+
+            }
+
+}
