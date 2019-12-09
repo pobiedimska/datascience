e683f74febd3acdc1d7f38999e10edd6d54acff5
Tim Dorotiuk <ok.usa.ny@gmail.com>
2019-12-09 20:49:10 +0200

    add dataset

---
+++ dorotiuktimdt/dataset_structure.py
@@ -0,0 +1,20 @@
+dataset = {
+"adidas_yeezy" : {'keys': 'mensshoes, yeezyboost,677898905645,677898056405',
+                'ean': '9.46989E+11',
+                'manufacturer': 'VietnamFabric',
+                'brand': 'ADIDAS'},
+
+
+"nike_airforce" : {'keys': 'basketballshoes, airforce1,847484756385,847484756154',
+                 'ean': '7.18483E+11',
+                 'manufacturer': 'BuffaloTricko',
+                 'brand': 'NIKE'},
+
+
+"puma_thunder" : {'keys': 'runningshoes, thundermodel,pumashoes, 673783484573,673783486548',
+                'ean': '8.56739E+11',
+                'manufacturer': 'Sports Manufacture Company. Limited',
+                'brand': 'PUMA'}
+}
+
+
