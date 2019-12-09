ea4bf53fc5ea43616052a7f87386ca5bcdf6aa33
Romanov-Niko <kolya.romanov8@gmail.com>
2019-12-09 15:07:08 +0200

    dataset_structure

---
+++ romanov_m_d/dataset_structure.py
@@ -0,0 +1,30 @@
+dataset = {
+    'AVpe__eOilAPnD_xSt-H': {
+        'id': 'AVpe__eOilAPnD_xSt-H',
+        'name': 'Handcrafted Alpaca Blend \'Purple Charisma\' Sweater (Peru)',
+        'price': '62.99',
+        'sizes': 'null'
+         },
+    'AVpe__eOilAPnD_xSt-H': {
+        'id': 'AVpe__eOilAPnD_xSt-H',
+        'name': 'Handcrafted Alpaca Blend \'Purple Charisma\' Sweater (Peru)',
+        'price': '62.99',
+        'sizes': 'null'
+         },
+    'AVpe__eOilAPnD_xSt-H': {
+        'id': 'AVpe__eOilAPnD_xSt-H',
+        'name': 'Handcrafted Alpaca Blend \'Purple Charisma\' Sweater (Peru)',
+        'price': '62.99',
+        'sizes': 'null'
+         }
+
+
+
+
+
+
+
+
+
+
+          }
