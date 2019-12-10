dcd4055fddee8684403d4c0a8fa9c2d8dc201c9f
artemoliinyk <a.oliinyk-2023@kpi.ua>
2019-12-10 20:55:42 +0200

    dataset_structure and description modified

--- oliinykao/dataset_structure.py
+++ oliinykao/dataset_structure.py
@@ -1,18 +1,15 @@
 dataset = {
-                "1 пара": {
-                            "id": "AVpe__eOilAPnD_xSt-H",
+                "AVpe__eOilAPnD_xSt-H": {
                             "brand": "Novica",
                             "colors": "Purple",
                             "flavors": ""
                           },
-                "2 пара": {
-                            "id": "AVpe_0ob1cnluZ0-bnWr",
+                "AVpe_0ob1cnluZ0-bnWr": {
                             "brand": "BEAUTIFEET",
                             "colors": "Multicolor,MULTI",
                             "flavors": ""
                           },
-                "3 пара": {
-                            "id": "AVpe_19KilAPnD_xSqnY",
+                "AVpe_19KilAPnD_xSqnY": {
                             "brand": "Caparros",
                             "colors": "Silver",
                             "flavors": ""
