85098e30aedcf659b499c2f049d89a6aa476cdb5
Romanov-Niko <55375904+Romanov-Niko@users.noreply.github.com>
2019-12-11 17:59:56 +0200

    Update dataset_structure.py

--- romanov_m_d/dataset_structure.py
+++ romanov_m_d/dataset_structure.py
@@ -1,30 +1,20 @@
 dataset = {
-    '1': {
+    'AVpe__eOilAPnD_xSt-H': {
         'id': 'AVpe__eOilAPnD_xSt-H',
         'name': 'Handcrafted Alpaca Blend \'Purple Charisma\' Sweater (Peru)',
         'price': '62.99',
         'sizes': 'null'
          },
-    '2': {
-        'id': 'AVpe__eOilAPnD_xSt-H',
-        'name': 'Handcrafted Alpaca Blend \'Purple Charisma\' Sweater (Peru)',
-        'price': '62.99',
+    'AVpe__JXilAPnD_xSt3F': {
+        'id': 'AVpe__JXilAPnD_xSt3F',
+        'name': 'Pedi Couture Women\'s Blue Snake Pedicure Spa Toe Separator Sandal Flip Flops (m)',
+        'price': '30',
         'sizes': 'null'
          },
-    '3': {
-        'id': 'AVpe__eOilAPnD_xSt-H',
-        'name': 'Handcrafted Alpaca Blend \'Purple Charisma\' Sweater (Peru)',
-        'price': '62.99',
+    'AVpe_0mrilAPnD_xSqI7': {
+        'id': 'AVpe_0mrilAPnD_xSqI7',
+        'name': 'Anne Klein Sport Perfered Women Us 9.5 Nude Wedge Heel',
+        'price': '100',
         'sizes': 'null'
          }
-
-
-
-
-
-
-
-
-
-
           }
