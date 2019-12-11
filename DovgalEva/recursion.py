2c335ff4af777bdad9a0cc0f03285d6a2ce2fabb
pinkLama <eclipse7723@gmail.com>
2019-12-11 18:26:55 +0200

    minor update

--- DovgalEva/recursion.py
+++ DovgalEva/recursion.py
@@ -1,5 +1,6 @@
 from DovgalEva.dataset_structure import dataset
 
+
 def recursion(data):
     if len(data) == 0:
         return 0
@@ -7,9 +8,9 @@ def recursion(data):
     condition = 0
     for key, value in data[0].items():
         if key == 'city' and value == 'IRWINDALE':
-            condition +=1
+            condition += 1
         elif key == 'total_episodes_non_lupa' and value > 1000:
-            condition +=1
+            condition += 1
         else: continue
     if condition == 2:
         print(data[0].get('provider_id'))
