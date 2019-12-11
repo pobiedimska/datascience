4ec07c019b20ffeedb8b9b218ce4a1b802cb5669
aAndrew <mirik23452@gmail.com>
2019-12-11 16:28:29 +0200

    with none

--- miroshnyk_a_v/validator/dataset_structure.py
+++ miroshnyk_a_v/validator/dataset_structure.py
@@ -1,23 +1,22 @@
-
 dataset={
     'Handcrafted Alpaca':{'name':{'brend':"Purple Charisma",
                  'type':"Sweater ",
                  'country':'Peru'},
          'size':13,
          'quantities':99,
-         'reviews':'none'},
+         'reviews':None},
     'Sof Comfort Womens Everyday Gel':{'name':{'brend':"Toe Canvas",
                  'type':"Sweater ",
                  'country':'Peru'},
          'size':14,
          'quantities':98,
-         'reviews':'none'},
+         'reviews':None},
     'Anne Klein Sport ':{'name':{'brend':"Toe Canvas",
                  'type':"Supastar Flat",
                  'country':'Ukrain'},
          'size':14,
          'quantities':9,
-         'reviews':'none'}}
+         'reviews':None}}
 
 
 
