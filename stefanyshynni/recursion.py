b38e63fe2f41992af30bc3267688486b84d98095
pinkLama <eclipse7723@gmail.com>
2019-12-11 11:20:48 +0200

    deleted trash

--- stefanyshynni/recursion.py
+++
@@ -1,9 +0,0 @@
-def recursion(dataset):
-    brends = list()
-    for i in range(len(dataset[0])):
-        if len(dataset[0][i]) > 10:
-            brends.append(dataset[0][i])
-
-    for j in range(len(brends)):
-        brend = brends[j]
-        size = dataset.values("brend", "")
