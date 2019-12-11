5a2824b979ea8c6ef19ed4c992e40fa18dbac818
MaximShcherbachenko <55317211+MaximShcherbachenko@users.noreply.github.com>
2019-12-11 11:42:15 +0200

    Dataset structure

---
+++ shcherbachenkoma/dataset_structure.py
@@ -0,0 +1,8 @@
+dataset = {
+    "provider_id": {
+        "agency_name": "ALL AMERICAN HEALTHCARE INC",
+        "average_hcc_score": 2.7
+        "percent_of_beneficiaries_with_asthma": 31
+        }
+    }
+        
