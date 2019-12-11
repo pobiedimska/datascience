64089c1c2be10de6d67fb851f43c2cab50db0dcd
egorkravchenko13 <56040497+egorkravchenko13@users.noreply.github.com>
2019-12-11 12:02:11 +0200

    Add files via upload

---
+++ egorkravchenko/dataset_structure.py
@@ -0,0 +1,17 @@
+dataset = {
+    "747203" : {
+        "zip_code" : "77036",
+        "distinct_beneficiaries_non_lupa" : "13",
+        "average_number_of_total_visits_per_episode_non_lupa" : "13.8",
+    }
+    "237745" : {
+        "zip_code" : "48075",
+        "distinct_beneficiaries_non_lupa" : "100",
+        "average_number_of_total_visits_per_episode_non_lupa" : "13.1"
+    }
+    "747900" : {
+        "zip_code" : "77071",
+        "distinct_beneficiaries_non_lupa" : "25",
+        "average_number_of_total_visits_per_episode_non_lupa" : "21.5",
+    }
+}
