8175813081d57773dfc9c637d021e5a4cd6002a1
pinkLama <eclipse7723@gmail.com>
2019-12-11 11:21:40 +0200

    task 1: description task 2: dataset

---
+++ DovgalEva/dataset_structure.py
@@ -0,0 +1,20 @@
+dataset = {
+    {
+        'provider_id': 337290,
+        'city': 'BROOKLYN',
+        'zip_code': 11234,
+        'total_episodes_non_lupa': 31483
+    },
+    {
+        'provider_id': 58217,
+        'city': 'IRWINDALE',
+        'zip_code': 91706,
+        'total_episodes_non_lupa': 620
+    },
+    {
+        'provider_id': 58419,
+        'city': 'BREA',
+        'zip_code': 92821,
+        'total_episodes_non_lupa': 19
+    }
+}
