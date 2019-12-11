4bbac14464cbcd23d4ef920f0a82610a0fbb9ccb
MaximShcherbachenko <55317211+MaximShcherbachenko@users.noreply.github.com>
2019-12-11 11:54:18 +0200

    Update dataset_structure.py

--- shcherbachenkoma/dataset_structure.py
+++ shcherbachenkoma/dataset_structure.py
@@ -1,17 +1,17 @@
 dataset = {
     "747203": {
         "agency_name": "ALL AMERICAN HEALTHCARE INC",
-        "average_hcc_score": 2.7
+        "average_hcc_score": 2.7,
         "percent_of_beneficiaries_with_asthma": 31
-        }
+        },
     "747016": {
         "agency_name": "ALL AMERICAN HEALTHCARE INC",
-        "average_hcc_score": 1.76
+        "average_hcc_score": 1.76,
         "percent_of_beneficiaries_with_asthma": 23
-        }
+        },
     "747145": {
         "agency_name": "ALL AMERICAN HEALTHCARE INC",
-        "average_hcc_score": 2.04
+        "average_hcc_score": 2.04,
         "percent_of_beneficiaries_with_asthma": 12
         }
     }
