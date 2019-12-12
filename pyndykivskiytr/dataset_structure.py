074abb1fe291fa9fcce3638cffcba5e21fb4dd05
taraspyndykivskiy <taraspyndykivskiy@gmail.com>
2019-12-12 21:39:32 +0200

    Taras Pyndykivskiy : 1 and 2 tasks completed : dataset structure and description created

---
+++ pyndykivskiytr/dataset_structure.py
@@ -0,0 +1,19 @@
+dataset = {
+    "747203" : {
+        "city" : "HOUSTON"
+        "total_lupa_episodes" : 0,
+        "average_age" : 52,
+    },
+
+    "747016" : {
+        "city" : "RICHMOND"
+        "total_lupa_episodes" : 0,
+        "average_age" : 53,
+    },
+
+    "747145" : {
+        "city" : "HOUSTON"
+        "total_lupa_episodes" : None,
+        "average_age" : 54,
+    },
+}
