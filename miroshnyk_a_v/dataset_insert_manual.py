abb6d45fe52f73c78e24ec9e00b7784e131c60d7
aAndrew <mirik23452@gmail.com>
2019-12-12 18:45:28 +0200

    dataset

--- miroshnyk_a_v/dataset_insert_manual.py
+++
@@ -1,8 +0,0 @@
-from miroshnyk_a_v.dataset_structure import dataset
-for val in dataset.values():
-     val.update({"name":{
-    'Handcrafted Alpaca Blend \'Purple Charisma\' Sweater (Peru)':{
-         'sizes':[14,15],
-         'quantities':24,
-         'reviews':"Not bad"}}})
-print(dataset)
