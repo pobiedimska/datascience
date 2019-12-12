163dbaf05fcd47a496139f1598e29a22e3ccbbd3
aAndrew <mirik23452@gmail.com>
2019-12-12 18:38:50 +0200

    dataset

---
+++ miroshnyk_a_v/dataset_structure.py
@@ -0,0 +1,13 @@
+dataset={"name":{
+    'Handcrafted Alpaca Blend \'Purple Charisma\' Sweater (Peru)':{
+         'sizes':[14,15],
+         'quantities':99,
+         'reviews':None},
+    'Marc Fisher Fawna Women Cap Toe Leather Oxford':{
+         'sizes':[14,34,16],
+         'quantities':98,
+         'reviews':None},
+    'Inc International Concepts Britanii Pointed Toe Suede Heels':{
+         'sizes':[14,16],
+         'quantities':9,
+         'reviews':None}}}
