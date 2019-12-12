e74e6fe55731862b7b63fcc94caec6c4a78b7598
artemoliinyk <a.oliinyk-2023@kpi.ua>
2019-12-12 12:41:28 +0200

    num5 v2

--- oliinykao/dataset_structure.py
+++ oliinykao/dataset_structure.py
@@ -1,17 +1,19 @@
 dataset = {
-                "AVpe__eOilAPnD_xSt-H": {
+               "id":{
+                   "AVpe__eOilAPnD_xSt-H": {
                             "brand": "Novica",
                             "colors": "Purple",
                             "flavors": None
                           },
-                "AVpe_0ob1cnluZ0-bnWr": {
+                    "AVpe_0ob1cnluZ0-bnWr": {
                             "brand": "BEAUTIFEET",
                             "colors": "Multicolor,MULTI",
                             "flavors": None
                           },
-                "AVpe_19KilAPnD_xSqnY": {
+                    "AVpe_19KilAPnD_xSqnY": {
                             "brand": "Caparros",
                             "colors": "Silver",
                             "flavors": None
                           }
+                    }
             }
