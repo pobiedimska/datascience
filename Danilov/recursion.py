bb693c50c62fde3499f55f7ecac4dd2b76f6c99f
Student <tereshchenko.igor@gmail.com>
2019-12-13 16:05:35 +0200

    create dataset_structure

--- Danilov/recursion.py
+++ Danilov/recursion.py
@@ -1,3 +1,9 @@
+def recurcion(dataset):
+    for host_id in dataset.keys():
+        if "bedrooms">=2 or roomtype == "Private room":
+            print("Host ID: "+str(host_id)+"\nroomtype: "+str(room_type)+"\ncountry: "+str(country))
+
+
 dataset = {
     "956883":
         {
@@ -10,7 +16,7 @@ dataset = {
         {
             "room_type":"Private room",
             "bedrooms":"1",
-            "country":"United States"
+            "country":"United States",
         },
     "25906186":
         {
@@ -19,3 +25,5 @@ dataset = {
             "country":"United States"
         }
      }
+
+print(recurcion(dataset))
