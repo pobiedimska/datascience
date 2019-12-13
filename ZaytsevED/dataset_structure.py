2470d205085e249932d00402fbe2c3f65ff9f126
Evgen Zaytsev <zond.jeka@gmail.com>
2019-12-13 16:14:27 +0200

    dataset

---
+++ ZaytsevED/dataset_structure.py
@@ -0,0 +1,17 @@
+dataset = {
+        '133214': {
+            'state': 'TX',
+            'white_beneficiaries' : 11,
+            'black_beneficiaries': 18
+        },
+        '765434': {
+            'state': 'OH',
+            'white_beneficiaries' : 14,
+            'black_beneficiaries': 46
+        },
+         '133214': {
+            'state': 'MI',
+            'white_beneficiaries' : 21,
+            'black_beneficiaries': 254
+         }
+}
