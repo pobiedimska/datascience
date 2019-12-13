ac670bf63a78cb013f534dba6ad95c5664954b79
mashapinda <masha.pinda@gmail.com>
2019-12-13 11:15:38 +0200

    Add dataset, description

---
+++ PindaMV/dataset_structure.py
@@ -0,0 +1,11 @@
+dataset = {
+            'PLATANUS ACERIFOLIA': {'cen_year': 2005
+                     , 'wire_other': 'No'
+                     , 'inf_other': 'No'}
+            , 'ULMUS SPECIES': {'cen_year': 2006
+                     , 'wire_other': 'No'
+                     , 'inf_other': 'No'}
+            , 'GLEDITSIA TRIACANTHOS': {'cen_year': 2006
+                     , 'wire_other': 'No'
+                     , 'inf_other': 'No'}
+            }
