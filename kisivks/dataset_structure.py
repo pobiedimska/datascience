25191973a320d7922243707870c36767698ea282
Katekisiv <katerinakisiv@gmail.com>
2019-12-12 19:48:37 -0700

    create dataset_structure

---
+++ kisivks/dataset_structure.py
@@ -0,0 +1,21 @@
+dataset = [
+    {
+        'provider_id': 747583,
+        'zip_code': 77407,
+        'percent_of_beneficiaries_with_ra_oa': 0,
+        'percent_of_beneficiaries_with_stroke': 5
+    },
+    {
+        'provider_id': 453165,
+        'zip_code': 77074,
+        'percent_of_beneficiaries_with_ra_oa': 73,
+        'percent_of_beneficiaries_with_stroke': 4
+    },
+    {
+        'provider_id': 679380,
+        'zip_code': 75116,
+        'percent_of_beneficiaries_with_ra_oa': 71,
+        'percent_of_beneficiaries_with_stroke': 21
+    }
+]
+
