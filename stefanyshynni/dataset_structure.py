30ca99918fd3327da850b77c3add022c6f2a3033
nazar0810 <nazarstefanyshyn63@gmail.com>
2019-12-13 20:24:18 +0200

    added validator

--- stefanyshynni/dataset_structure.py
+++ stefanyshynni/dataset_structure.py
@@ -1,29 +1,29 @@
 dataset = {
-    "brand":{
-        "Marc Fisher":{
-            "name": "Marc Fisher Fawna Women Cap Toe Leather Oxford",
-            "sizes":{
-                8.5,7,9.5,7.5,8,
+    'brand':{
+        'Marc Fisher':{
+            'name': 'Marc Fisher Fawna Women Cap Toe Leather Oxford',
+            'sizes':{
+                8.5,7,9.5,7.5,8
             },
-            "merchants":
-                "Apparelsave"
+            'merchants':
+                'Apparelsave'
         },
 
-        "Caparros":{
-            "name":
-                "Caparros Karissa Open Toe Canvas Sandals",
-            "sizes":{
+        'Caparros':{
+            'name':
+                'Caparros Karissa Open Toe Canvas Sandals',
+            'sizes':{
                 8.5, 7.5, 8
             },
-            "merchants": "Apparelsave"
+            'merchants': 'Apparelsave'
         },
 
-        "DbDk Fashion":{
-            "name": "Dbdk Fashion Meira-4 Women Open Toe Synthetic Wedge Sandall",
-            "sizes": {
+        'DbDk Fashion':{
+            'name': 'Dbdk Fashion Meira-4 Women Open Toe Synthetic Wedge Sandall',
+            'sizes': {
                 5.5,7,6,6.5,7.5,8
             },
-            "merchants": "Apparelsave"
+            'merchants': 'Apparelsave'
         }
     }
 }
--- stefanyshynni/validator/lib.py
+++ stefanyshynni/validator/lib.py
@@ -1,3 +1,42 @@
+import re
+
 def name_validator():
-    pass
+    name = input("Введіть назву:")
+    if bool(re.match("^(([A-Z]{1}([a-z]+)?((\')?[a-z]+)?(\,)?(\s)?)+)?$", name)):
+        pass
+    else:
+        print("Назва введена неправильно!")
+        return name_validator()
+    name.split()
+    return name
+
+def brand_validator():
+    brand = input("Введіть бренд:")
+    if bool(re.match("^(([A-Z]+([a-z]+)?((\')?[a-z]+)?(\s)?(\&)?(\s)?)+)?$", brand)):
+        pass
+    else:
+        print("Бренд введена неправильно!")
+        return brand_validator()
+    brand.split()
+    return brand
+
+def sizes_validator():
+    sizes = input("Введіть розміри взуття:")
+    if bool(re.match("^([0-9]+((\.)?[0-9]+)?(\,)?(\s)?)+$", sizes)):
+        pass
+    else:
+        print("Розміри введено неправильно!")
+        return sizes_validator()
+    sizes.split()
+    return sizes
+
+def merchants_validator():
+    merchants = input("Покупець:")
+    if bool(re.match("^(([A-Z]+([a-z]+)?((\')?([a-z]+)?(\.)?[a-z]+)?(\s)?)+)?$", merchants)):
+        pass
+    else:
+        print("Інформація  введена неправильно!")
+        return merchants_validator()
+    merchants.split()
+    return merchants
 
