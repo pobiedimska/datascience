8585a1fc0ca0f854080591761ad11f5004f0cfba
Your Name <you@example.com>
2019-12-13 11:31:49 +0200

    dataset_structure

--- Lutak MV/dataset_structure.py
+++ Lutak MV/dataset_structure.py
@@ -1,14 +1,17 @@
 dt = {
-      'spc_latin': {'PLATANUS ACERIFOLIA'
-                   ,'ULMUS SPECIES'
-                   ,'GREDITSIA TRIACANTHOS'
-                    }
-      ,'zipcode': {10471
-                  ,11432
-                  ,10007
-                   }
-      ,'inf_conopy': {'No'
-                      ,'No'
-                      ,'No'
-                      }
+      tree1:{
+            'spc_latin':'PLATANUS ACERIFOLIA'
+           ,'zipcode': 10471
+           ,'inf_conopy': 'No'
+            }
+      tree2:{
+             'spc_latin': 'ULMUS SPECIES'
+            ,'zipcode': 11432
+            ,'inf_conopy': 'No'
+            }
+      tree3:{
+             'spc_latin': 'GREDITSIA TRIACANTHOS'
+            ,'zipcode': 10007
+            ,'inf_conopy': 'No'
+            }
       }
