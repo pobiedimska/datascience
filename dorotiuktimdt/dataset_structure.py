aaacfd2ccc20382933d19b25fb1ed86fa5c2727e
Tim Dorotiuk <ok.usa.ny@gmail.com>
2019-12-13 17:45:06 +0200

    added dataset structure

--- dorotiuktimdt/dataset_structure.py
+++ dorotiuktimdt/dataset_structure.py
@@ -1,20 +1,15 @@
 dataset = {
-"adidas_yeezy" : {'keys': 'mensshoes, yeezyboost,677898905645,677898056405',
-                'ean': '9.46989E+11',
-                'manufacturer': 'VietnamFabric',
-                'brand': 'ADIDAS'},
+    'ean': {'9.46989E+11':{
+                        'keys': 'mensshoes, 677898905645 / yeezyboost, 677898056405',
+                        'manufacturer': 'VietnamFabric',
+                        'brand': 'ADIDAS'},
+            '7.18483E+11':{
+                         'keys': 'basketballshoes, 847484756385/ airforce1, 847484756154',
+                         'manufacturer': 'BuffaloTricko',
+                         'brand': 'NIKE'},
+            '8.56739E+11': {
+                          'keys': 'runningshoes, 673783484573 / thundermodel, 673783486548',
+                          'manufacturer': 'Sports Manufacture Company.Limited',
+                           'brand': 'PUMA'}}
 
-
-"nike_airforce" : {'keys': 'basketballshoes, airforce1,847484756385,847484756154',
-                 'ean': '7.18483E+11',
-                 'manufacturer': 'BuffaloTricko',
-                 'brand': 'NIKE'},
-
-
-"puma_thunder" : {'keys': 'runningshoes, thundermodel,pumashoes, 673783484573,673783486548',
-                'ean': '8.56739E+11',
-                'manufacturer': 'Sports Manufacture Company. Limited',
-                'brand': 'PUMA'}
 }
-
-
