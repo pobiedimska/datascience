3bea122b945be14953834442f31d2b21cd2f38a2
ViktoriiaHudym <vikahudym222@gmail.com>
2019-12-13 21:25:44 +0200

    Validator

---
+++ hudymvm/validator/lib.py
@@ -0,0 +1,21 @@
+import re
+
+def id_validator(prompt):
+    id = input(prompt)
+    if bool(re.match('AVpe_{1}[-_A-Za-z0-9]{15}', id))!=True:
+        id = input(prompt)
+
+def categories_validator(prompt):
+    categories = input(prompt)
+    if bool(re.match("(([A-Z]{1}[a-z`' ]{1,}\,{0,1}){1,}|(none|None))", categories))!=True:
+        categories = input(prompt)
+
+def keys_validator(prompt):
+    keys = input(prompt)
+    if bool(re.match('([a-z\/,.0-9]{1,}|(none|None))', keys))!=True:
+        keys = input(prompt)
+
+def name_validator(prompt):
+    name = input(prompt)
+    if bool(re.match("(([A-Z]{1}[A-Za-z'().,/-0-9 ]{1,}\s{0,1}){1,}|(none|None))",name))!=True:
+        name = input(prompt)
---
+++ hudymvm/validator/test.py
@@ -0,0 +1,6 @@
+from hudymvm.validator.lib import id_validator, name_validator, categories_validator, keys_validator
+
+id = id_validator('Enter id ')
+categories = categories_validator('Enter categories ')
+keys = keys_validator('Enter keys ')
+name = name_validator('Enter name ')
