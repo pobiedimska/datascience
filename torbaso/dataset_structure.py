4c649d581e189226796598ed527e431175f85e8c
Your Name <you@example.com>
2019-12-13 15:22:51 +0200

    My Descriotion and Dataset

---
+++ torbaso/dataset_structure.py
@@ -0,0 +1,23 @@
+dataset = {
+    'Stylish Queen Anne Apartment':{
+        'country':'United States',
+        'bed-type':'Real Bed',
+        'price':'$85.00'
+    },
+    'Bright & Airy Queen Anne Apartment':{
+        'country':'United States',
+        'bed-type':'Real Bed',
+        'price':'$150.00'
+    },
+    'New Modern House - Amazing water view':{
+        'country':'United States',
+        'bed-type':'Real Bed',
+        'price':'$975.00'
+    }
+}
+
+
+
+
+
+
