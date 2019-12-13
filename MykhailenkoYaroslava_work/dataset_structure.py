68092089a4b68fab51feaa54885360aa9f637101
Your Name <you@example.com>
2019-12-13 11:52:45 +0200

    dataset_Mikhailenko

---
+++ MykhailenkoYaroslava_work/dataset_structure.py
@@ -0,0 +1,12 @@
+city_info ={
+    "status":{
+        "crown":"Excellent",
+        "deformities":"Poor",
+        "leaves absent":"Dead"
+    },
+    "horz_plant":{
+        "London planetree":False,
+        "Unknown":True,
+        "honeylocust":False
+    }
+}
