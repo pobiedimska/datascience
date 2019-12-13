1304d0a1048b3d9744393578f535cfe8bf27f459
Nikita <mykytamaliarenko@gmail.com>
2019-12-13 11:00:15 +0200

    create dataset_structure

---
+++ maliarenkoma/dataset_structure.py
@@ -0,0 +1,28 @@
+SPC_COMMON, INF_OTHER, CEN_YEAR, ZIP_CITY = ("spc_common", "inf_other", "cen_year", "zip_city")
+
+dataset = [
+    {
+        CEN_YEAR: 2005,
+        SPC_COMMON: "LONDON PLANETREE",
+        INF_OTHER: False,
+        ZIP_CITY: "Bronx"
+    },
+    {
+        CEN_YEAR: 2065,
+        SPC_COMMON: "ELM, OTHER",
+        INF_OTHER: False,
+        ZIP_CITY: "Jamaica"
+    },
+    {
+        CEN_YEAR: 2005,
+        SPC_COMMON: "MAPLE, NORWAY",
+        INF_OTHER: True,
+        ZIP_CITY: "Flushing"
+    },
+    {
+        CEN_YEAR: 2006,
+        SPC_COMMON: "PLUM, PURPLELEAF",
+        INF_OTHER: False,
+        ZIP_CITY: "Middle Village"
+    },
+]
