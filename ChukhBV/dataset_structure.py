22df3ffb386b24c5f9be17de667804f35efba36f
Bodia <lalala@gmail.com>
2019-12-13 15:32:10 +0200

    create dataset_structure

---
+++ ChukhBV/dataset_structure.py
@@ -0,0 +1,18 @@
+dataset={
+    "7421966":{
+        "neighbourhood":"Queen Anne",
+        "state":"WA",
+        "country":"United States"
+    },
+    "1266459":{
+        "neighbourhood":"Ballard",
+        "state":"WA",
+        "country":"United States"
+    },
+    "6202214":{
+        "neighbourhood":"Wallingford",
+        "state":"WA",
+        "country":"United States"
+    }
+
+}
