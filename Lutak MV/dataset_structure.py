2c65929a0414d8cca6f4fde7ee912bcdceae0bd2
Your Name <you@example.com>
2019-12-13 11:18:14 +0200

    dataset_structure

---
+++ Lutak MV/dataset_structure.py
@@ -0,0 +1,14 @@
+dt = {
+      'spc_latin': {'PLATANUS ACERIFOLIA'
+                   ,'ULMUS SPECIES'
+                   ,'GREDITSIA TRIACANTHOS'
+                    }
+      ,'zipcode': {10471
+                  ,11432
+                  ,10007
+                   }
+      ,'inf_conopy': {'No'
+                      ,'No'
+                      ,'No'
+                      }
+      }
