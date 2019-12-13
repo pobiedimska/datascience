d92668b781411f60c7afa672bd0bcb0a4589559c
Drobyazko <philipjava1111@gmail.com>
2019-12-13 22:09:02 +0200

    dataset_structure

--- DrobyazkoOV/dataset_structure.py
+++ DrobyazkoOV/dataset_structure.py
@@ -1,16 +1,16 @@
 dataset = {
-        "AVpe__eOilAPnD_xSt-H": {
-            "name": "Handcrafted1",
+        "Boots": {
+            "id": "1234qw_er23",
             "prices.offer": {46, 567, 453},
             "size": {41, 42, 43, 44}
         },
-        "AVpe__eOilAPnD_xSt": {
-            "name": "Handcrafted2",
+        "Hills": {
+            "id": "AVpe__eOilAPnD_xSt",
             "prices.offer": {478, 57, 4531},
             "size": {41, 42, 44}
         },
-        "AVpe__eOilAPnD_t-H": {
-            "name": "Handcrafted3",
+        "Shoes": {
+            "id": "AVpe__eOilAPnD_t-H",
             "prices.offer": {46, 567, 453},
             "size": {41, 42, 43, 44}
         },
