9959458ae5a625e0c1e8cc4f1797d2201a37601b
asya <qwerty.com>
2019-12-13 15:45:01 +0200

    task1, task2

---
+++ Rodina/dataset_structure.py
@@ -0,0 +1,22 @@
+dataset = {
+     956883:
+          {
+        'guest-included': 2,
+        'amenities':{'TV',"Cable TV",'Internet',"Wireless Internet","Air Conditioning",
+                     'Kitchen','Heating',"Family/Kid Friendly",'Washer','Dryer'},
+        'room-type': "Entire home"
+         },
+     326758:
+          {
+        'guest-included': 1,
+        'amenities':{"Wireless Internet","Free Parking on Premises","Heating","Smoke Detector","Essentials","Shampoo"},
+        'room-type':'Private room'
+         },
+     2497928:
+          {
+        'guest-included': 1,
+        'amenities':{"Wireless Internet","Free Parking on Premises",
+                    'Heating',"Smoke Detector","First Aid Kit",'Essentials','Shampoo'},
+        'room-type': 'Private room'
+         }
+          }
