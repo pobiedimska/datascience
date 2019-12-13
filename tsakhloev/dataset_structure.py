03d0edfaf25efabf67f32a4ccc13ba31dfd48e8c
Your Name <you@example.com>
2019-12-13 14:44:26 +0200

    dataset_structure

---
+++ tsakhloev/dataset_structure.py
@@ -0,0 +1,27 @@
+dataset = {
+
+    4016632: {
+
+        'cleaning-fee': '$25.00',
+        'calendar-updated': 'yesterday',
+        'extra-people': '$0.00'
+
+    },
+
+    9851441: {
+
+        'cleaning-fee': None,
+        'calendar-updated': '6 months ago',
+        'extra-people': '$0.00'
+
+    },
+
+    239585: {
+
+    	'cleaning-fee': '$15.00',
+        'calendar-updated': 'a week ago',
+        'extra-people': '$15.00'
+
+    }
+
+}
