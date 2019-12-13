9b80a241a5f59ea3c06c5ac7b6d1158f140f03b3
Moonlight <kolya.romanov8@gmail.com>
2019-12-13 18:34:17 +0200

    validator

--- romanovmd/dataset_structure.py
+++ romanovmd/dataset_structure.py
@@ -2,19 +2,19 @@ dataset = {
     'AVpe__eOilAPnD_xSt-H': {
         'id': 'AVpe__eOilAPnD_xSt-H',
         'name': 'Handcrafted Alpaca Blend \'Purple Charisma\' Sweater (Peru)',
-        'price': '62.99',
-        'sizes': 'null'
+        'price.amountMin': '62.99',
+        'sizes': 'None'
          },
     'AVpe__JXilAPnD_xSt3F': {
         'id': 'AVpe__JXilAPnD_xSt3F',
         'name': 'Pedi Couture Women\'s Blue Snake Pedicure Spa Toe Separator Sandal Flip Flops (m)',
-        'price': '30',
-        'sizes': 'null'
+        'price.amountMin': '30',
+        'sizes': 'None'
          },
     'AVpe_0mrilAPnD_xSqI7': {
         'id': 'AVpe_0mrilAPnD_xSqI7',
         'name': 'Anne Klein Sport Perfered Women Us 9.5 Nude Wedge Heel',
-        'price': '100',
-        'sizes': 'null'
+        'price.amountMin': '100',
+        'sizes': 'None'
          }
           }
