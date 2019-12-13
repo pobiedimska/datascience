8d470136c33d841158b7fe35c00336f6c4f4f39f
Andrey <andronm93@gmail.com>
2019-12-13 15:42:27 +0200

    I added a text description document and a file dataset_structure

---
+++ Saltykov Andrey/dataset_structure.py
@@ -0,0 +1,21 @@
+dataset = {
+
+    1: {
+        'room_type': 'Private room',
+        'price': '$85.00',
+        'weekly_price': '$852.00',
+        'beds': 1
+    },
+    2: {
+        'room_type': 'Entire home/apt',
+        'price': '$8.00',
+        'weekly_price': '$815.00',
+        'beds': 2
+    },
+    3: {
+        'room_type': 'Entire home/apt',
+        'price': '$5.00',
+        'weekly_price': '$835.00',
+        'beds': 3
+    }
+}
