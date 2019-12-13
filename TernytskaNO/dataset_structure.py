a31f86595b46f91375c163a4ebf6fa153850fc3f
Your Name <you@example.com>
2019-12-13 11:47:07 +0200

    dataset_creation

---
+++ TernytskaNO/dataset_structure.py
@@ -0,0 +1,17 @@
+tree_census = {
+    "Platanus Acerifolia": {
+        "status": "good"
+        ,"cen-year": 2005
+        ,"wire-prime": "No"
+                           },
+    "Ulmus species":       {
+        "status": "good"
+        ,"cen-year": 2006
+        ,"wire-prime": "No"
+                            },
+    "Gleditsia Triacantos": {
+        "status": "excellent"
+        ,"cen-year": 2006
+        ,"wire-prime": "No"
+                            }
+              }
