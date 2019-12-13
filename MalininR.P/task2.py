d285387455d505cf5aa1d9c4f8c969882c617309
Malinin Rostislav <rarka333@gmail.com>
2019-12-13 15:44:35 +0200

    Add discription and task 2

---
+++ MalininR.P/task2.py
@@ -0,0 +1,22 @@
+
+
+dataset = {
+    "Stylish Queen Anne Apartment" :{
+        "host_name" : "Maija" ,
+        "street" : "Gilman Dr W, Seattle, WA 98119, United States",
+        "host_response_time" : "within a few hours"
+    },
+
+    "Bright & Airy Queen Anne Apartment":{
+        "host_name" : "Andrea",
+        "street" : "7th Avenue West, Seattle, WA 98119, United States",
+        "host_response_time" : "within a few hours",
+    },
+
+    "New Modern House-Amazing water view" :{
+        "host_name" : "Jill",
+        "street" : "West Lee Street, Seattle, WA 98119, United States",
+        "host_response_time" : "within a few hours"
+    }
+
+}
