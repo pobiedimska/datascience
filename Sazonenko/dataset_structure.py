86f0b9594fbef5449f2dfe7d7cff7eaa6cbd7e7f
eugene <sazoneugene04@gmail.com>
2019-12-13 11:27:02 +0200

    added description.txt with description of atributes added example of dataset

---
+++ Sazonenko/dataset_structure.py
@@ -0,0 +1,12 @@
+dataset = {
+    'PLATANUS ACERIFOLIA': {'status': 'good',
+                            'wire_htap': 'Yes',
+                            'borocode': 2},
+    'ULMUS SPECIES' : {'status': 'good',
+                       'wire_htap': 'No',
+                       'borocode': 4},
+    'GLEDITSIA TRIACANTHOS' : {'status':
+                                   'excellent',
+                               'wire_htap': 'No',
+                               'borocode': 1}
+}
