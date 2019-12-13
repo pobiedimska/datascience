9e9a4aa2fc202f920c2893841f632a6035ee97a5
Your Name <you@example.com>
2019-12-13 11:25:13 +0200

    Commit

---
+++ Oleh Sydorov/dataset_structure.py
@@ -0,0 +1,7 @@
+dataset_axample = {
+                    'LONDON PLANETREE':{
+                                        'spc_latin': 'PLATANUS ACERIFOLIA'
+                                        ,'status': 'Good'
+                                        ,'pit type': 'Lawn'
+                                                            }
+                    }
