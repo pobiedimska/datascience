0bd1f9ad95dd9c9490b51a87e51787efd43d8c04
marim <marimat2014@gmail.com>
2019-12-13 11:28:23 +0100

    new message

---
+++ Matvienko/dataset_structure.py
@@ -0,0 +1,20 @@
+dataset={
+    "2005":{
+                "horz_plant": "No" ,
+                 "vert_wall":"No" ,
+                "sidw_raise":"No"
+            },
+
+    "2005": {
+            "horz_plant":"No",
+            "vert_wall":"No",
+            "sidw_raise":"No"
+            },
+
+    "2005": {
+                "horz_plant":"No",
+                "vert_wall":"No",
+                "sidw_raise":"No"
+                }
+
+}
