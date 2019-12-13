fbcb9d9a137bc8ab127ba9d4c43b141094492e91
Your Name <you@example.com>
2019-12-13 15:51:16 +0200

    Added description.txt and dataset_structure.py

---
+++ bespalovad/dataset_structure.py
@@ -0,0 +1,26 @@
+dataset = {
+    '1':{
+        'host_url': 'wewed',
+        'host_since': 'edwde',
+        'country': 'edded',
+        'zipcode': 'ewfwfe',
+    }
+    '2':{
+        'host_url': 'dfgvwef',
+        'host_since': 'gdefvg',
+        'country': 'fgvdef',
+        'zipcode': 'eewdf',
+    }
+    '3':{
+        'host_url': 'wewd',
+        'host_since': 'edegbde',
+        'country': 'edfgvdscv',
+        'zipcode': 'ewfwefw',
+    }
+    '4':{
+        'host_url': 'wevgdsf',
+        'host_since': 'easfsd',
+        'country': 'asdf',
+        'zipcode': 'eqwerrttyy',
+    }
+}
