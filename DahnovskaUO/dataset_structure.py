41cd0a8024698a98288a7c0cabcc4294e89df564
UlianaDahnovska <55241559+UlianaDahnovska@users.noreply.github.com>
2019-12-13 15:37:20 +0200

    added descriptions and task2

---
+++ DahnovskaUO/dataset_structure.py
@@ -0,0 +1,23 @@
+dataset = {
+    '241032':
+        {
+        'country_code':'US',
+        'city':'Seattle',
+        'street':'Gilman Dr W, Seattle, WA 98119, United States',
+        'price':'$85.00'
+        },
+    '953595':
+        {
+        'country_code':'US',
+        'city':'Seattle',
+        'street':'7th Avenue West, Seattle, WA 98119, United States',
+        'price':'$150.00'
+        },
+    '3308979':
+        {
+        'country_code':'US',
+        'city':'Seattle',
+        'street':'West Lee Street, Seattle, WA 98119, United States',
+        'price':'$975.00'
+        }
+    }
