bd8c7d1b7e0d3476ddf28028d9bdcf10ba7aac1e
dmytrob <krolegyb@gmail.com>
2019-12-13 20:01:43 +0200

    Fixed dataset, completed recursion, added validators

--- burdenkods/dataset_structure.py
+++ burdenkods/dataset_structure.py
@@ -1,23 +1,21 @@
 # keys - string
 # asins - UUID ?
 # name - string
-# size - string
+# size - float
 
 ShoeData = {
-    "ID0000001": {
-        "keys": "amazon.com/listing/123456",
+    "amazon.com/listing/123456": {
         "asins": None,
         "name": "Vintage Louboutin, worn",
         "size": "8"
     },
-    "ID0000002": {
-        "keys": "amazon.com/listing/123488876",
+    ""
+    "": {
         "asins": None,
         "name": "Authentic Tory Burch",
         "size": "7.5"
     },
-    "ID0000003": {
-        "keys": "walmart.com/listing/123456",
+    "walmart.com/listing/123456": {
         "asins": None,
         "name": "Basic Chelsea Boot",
         "size": "7.5"
