4f1d1e9b00d9db3fced7dea47b7bcc122619f811
taraspyndykivskiy <taraspyndykivskiy@gmail.com>
2019-12-13 22:29:56 +0200

    dataset expanded and recursion added

--- pyndykivskiytr/dataset_structure.py
+++ pyndykivskiytr/dataset_structure.py
@@ -1,19 +1,38 @@
 dataset = {
     "747203" : {
-        "city" : "HOUSTON"
+        "city" : "HOUSTON",
         "total_lupa_episodes" : 0,
         "average_age" : 52,
     },
 
     "747016" : {
-        "city" : "RICHMOND"
+        "city" : "RICHMOND", 
         "total_lupa_episodes" : 0,
         "average_age" : 53,
     },
 
     "747145" : {
-        "city" : "HOUSTON"
-        "total_lupa_episodes" : None,
+        "city" : "HOUSTON",
+        "total_lupa_episodes" : 0,
         "average_age" : 54,
+    },
+
+    "747429" : {
+        "city" : "MISSOURI CITY",
+        "total_lupa_episodes" : 0,
+        "average_age" : 54,
+    },
+    
+    "109763" : {
+        "city" : "NORTH MIAMI BEACH",
+        "total_lupa_episodes" : 34,
+        "average_age" : 54,
+    },
+
+    
+    "237745" : {
+        "city" : "SOUTHFIELD",
+        "total_lupa_episodes" : 49,
+        "average_age" : 56,
     },
 }
