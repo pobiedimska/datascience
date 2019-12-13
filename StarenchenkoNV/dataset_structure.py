2b06efd9a925754381676a105b7e5808c8b8fffa
nazarStarenchenko <nazarstarenchenko@gmail.com>
2019-12-13 11:22:14 +0200

    commiting dataset

--- StarenchenkoNV/dataset_structure.py
+++ StarenchenkoNV/dataset_structure.py
@@ -1,24 +1,8 @@
 dataset = {
 
-    "column1" : {
+    "0": {
     "sidw_crack":"No"
     ,"wire_prime":"No"
     ,"inf_shoes":"No"
-    ,"cb_num":0
-
-    },
-    "column2": {
-    "sidw_crack":"No"
-    ,"wire_prime":"No"
-    ,"inf_shoes":"No"
-    ,"cb_num":0
-
-    },
-    "column3": {
-    "sidw_crack":"No"
-    ,"wire_prime":"No"
-    ,"inf_shoes":"No"
-    ,"cb_num":0
-
     }
 }
