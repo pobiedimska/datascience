2d2ff573a189b89fa4626fcf77c48bc49d9e98be
asya <qwerty.com>
2019-12-13 11:16:32 +0200

    Опис атрибутів та приклад структури dataset

---
+++ venv/RyzhkovaDO/dataset_structure.py
@@ -0,0 +1,22 @@
+dataset = {
+           '1':{
+                'objectid': "698612"
+                ,'vert_wall': "No"
+                ,'zipcode': "10471"
+                ,'borocode': "2"
+                }
+           ,'2':{
+                'objectid': "860288"
+                ,'vert_wall': "No"
+                ,'zipcode': "11432"
+                ,'borocode': "4"
+                }
+           ,'3':{
+                'objectid': "641807"
+                ,'vert_wall': "No"
+                ,'zipcode': "10007"
+                ,'borocode': "1"
+                }
+
+
+           }
