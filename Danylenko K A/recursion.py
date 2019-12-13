82a3caf56406cb7e13f307ccb6fe0dba0b5076a0
Your Name <you@example.com>
2019-12-13 11:26:10 +0200

    recursion func added

---
+++ Danylenko K A/recursion.py
@@ -0,0 +1,34 @@
+city_info = {
+    'new_jercy' : {
+        'vert_wall': True,
+        'wire_2nd': 200,
+        'tree_loc': dict(),
+        'cen_year': 2018,
+    },
+
+    'brooklyn': {
+        'vert_wall': False,
+        'wire_2nd': 1,
+        'tree_loc': dict(),
+        'cen_year': 2006,
+    },
+
+    'jercy': {
+        'wire_2nd' : 222,
+        'cen_year' : 2018
+
+    }
+
+}
+
+my_date = int(input("Введите год для поиска:\n"))
+
+def sec_wires_count(dataset,year):
+    total_sum = 0
+    for attributes in dataset.values():
+        if 'cen_year' in attributes:
+            if attributes['cen_year'] == year:
+                total_sum += attributes['wire_2nd']
+    return total_sum
+
+print('Количество проводов в 2018г:',sec_wires_count(city_info,my_date))
