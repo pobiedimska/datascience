a79c95cb1a9ecbd89419882afa673f9faf297d3e
pankeeva <pankeeva@gmail.com>
2019-12-14 23:22:24 +0200

    adding recursion, validator

---
+++ PankeyevaSD/validator/lib.py
@@ -0,0 +1,15 @@
+import re
+def id_validator():
+   return bool(re.match('^\d{6}$',id))
+
+
+def zip_code_validator(zip):
+    return bool(re.match('^\d{5}$', zip))
+
+
+def percent_of_beneficiaries_with_copd_validator(copd):
+   return bool(re.match("^([0-9]{1,2})?$",copd)) or bool(re.match('^([1]{1}[0]{1}[0]{1})?$',copd))
+
+
+def percent_of_beneficiaries_with_hypertension_validator(hypertension):
+    return bool(re.match("^([1]?[0-9]{1,2})?$", hypertension))
