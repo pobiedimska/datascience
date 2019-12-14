81cd71def2804288aaad9ce3f63a2957db6b77a4
taraspyndykivskiy <taraspyndykivskiy@gmail.com>
2019-12-14 19:30:00 +0200

    recursion function remade

--- pyndykivskiytr/recursion.py
+++ pyndykivskiytr/recursion.py
@@ -1,11 +1,3 @@
-"""
-def info(dataset):
-	print("Table info that satisfies conditions : ")
-	print("\nProvider id\t\tCity\t\tTotal lupa episodes")
-	for provider_id in dataset.keys():
-		if(dataset[provider_id]["average_age"]>30) and (dataset[provider_id]["total_lupa_episodes"]>2):
-			print(provider_id + '\t\t   ' + dataset[provider_id]["city"] + ' '*(30-len(dataset[provider_id]["city"])) + str(dataset[provider_id]["total_lupa_episodes"]))
-"""
 from dataset_structure import dataset
 
 def info(dataset, iterator=0):
