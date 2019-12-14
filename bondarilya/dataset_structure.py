882eaaa2a44dd7662db5c8c865c5b9d4004754b8
Bondar Ilya <bond3684@gmail.com>
2019-12-14 18:54:08 +0200

    added dataset and description

---
+++ bondarilya/dataset_structure.py
@@ -0,0 +1,17 @@
+dataset = {
+    "24578" : {
+        "state" : "TX",
+        "other_unknown_beneficiaries" : "0",
+        "percent_of_beneficiaries_with_alzheimers" : "29"
+    },
+    "53467" : {
+        "state" : "FL",
+        "other_unknown_beneficiaries" : "0",
+        "percent_of_beneficiaries_with_alzheimers" : "7"
+    },
+    "32572": {
+        "state" : "IL",
+        "other_unknown_beneficiaries" : "0",
+        "percent_of_beneficiaries_with_alzheimers" : "16"
+    }
+}
