82ca09f87cb4f677f7dc5cac9df2e4be95b892cd
Святослав <swyat.usatov@gmail.com>
2019-12-14 16:18:36 +0200

    new dataset(dataset_structure), new description

--- usatovsv/dataset_structure.py
+++ usatovsv/dataset_structure.py
@@ -2,19 +2,19 @@ dataset = {
     "10471" : {
         "cb_num": 0,
         "wire_2nd": False,
-        "spc-latin":"PLATANUS ACERIFOLIA"
+        "spc_latin":"PLATANUS ACERIFOLIA"
 
     },
-    "10307	" : {
+    "10307" : {
         "cb_num":0,
         "wire_2nd": True,
-        "spc-latin":"PYRUS CALLERYANA"
+        "spc_latin":"PYRUS CALLERYANA"
 
     },
     "11432" : {
         "cb_num":0,
         "wire_2nd": False,
-        "spc-latin": "ULMUS SPECIES"
+        "spc_latin": "ULMUS SPECIES"
 
     }
 }
