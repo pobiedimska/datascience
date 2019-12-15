04668e707bd98a23717b233d0be7acdf1f44f096
igortereshchenko <tereshchenko.igor@gmail.com>
2019-12-15 16:49:12 +0200

    my programs

--- PoliatskaAA/dataset_structure.py
+++ PoliatskaAA/dataset_structure.py
@@ -1,18 +1,24 @@
 dataset = {
             "AVpe__e0ilAPnD_xSt-H": {
-                                     "color": "black",
+                                     "color": {
+                                                "black"
+                                                },
                                      "ean": "980982193",
                                      "flavor": ''
                                       },
 
             "AVpe__hvg3456kFGk_jk": {
-                                    "color": "purple",
+                                    "color": {
+                                                "purple"
+                                                },
                                     "ean": "981112193",
                                     "flavor": ''
                                     },
 
             "VHGk_gu-h87iud": {
-                                "color": "red",
+                                "color": {
+                                            "red"
+                                            },
                                 "ean": "4000982193",
                                 "flavor": ''
                              }
