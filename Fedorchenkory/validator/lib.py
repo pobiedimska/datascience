13a0158ba30810112eda14a305fbc814e787407f
nazar0810 <nazarstefanyshyn63@gmail.com>
2019-12-16 01:14:01 +0200

    added intergration package and and an advanced peer student validator

--- Fedorchenkory/validator/lib.py
+++ Fedorchenkory/validator/lib.py
@@ -1,8 +1,12 @@
 
 import re
-def count_validator(count):
-    while not re.match('^[1-9]{1}[0-9]*$',count) :
-        count = input('enter correct count \n')
+def count_validator():
+    count = input('Введіть кількість одиниць товару:')
+    if  bool(re.match('^[1-9]{1}[0-9]*$',count)):
+        pass
+    else:
+        print('Неправильно введено кількість одиниць товару')
+        return count_validator()
     return count
 
 
@@ -12,11 +16,20 @@ def name_validator(name):
     return name
 
 
-def reviews_validator(reviews):
-    while not re.match('^[1-9]{1}[0-9]*$',reviews) :
-        reviews = input('enter correct reviews \n')
+def reviews_validator():
+    reviews = input('Введіть кількість переглядів товару:')
+    if bool(re.match('^[1-9]{1}[0-9]*$',reviews)):
+        pass
+    else:
+        print('Неправильно введено кількість переглядів товару')
+        return reviews_validator()
     return reviews
-def td_validator(td):
-    while not re.match('^[0-9 a-z A-Z]*$',td) :
-        td = input('enter correct reviews \n')
+
+def td_validator():
+    td = input('Введіть  унікальний номер рядку:')
+    if bool(re.match('^([A-Z]*[a-z]*(\_){1,2}(.)*)$',td)):
+        pass
+    else:
+        print('Неправильно введено унікальний номер рядку')
+        return td_validator()
     return td
