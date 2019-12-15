23dc4c4647ddf38c21b6a2202970bb2cf1901c66
UlianaDahnovska <55241559+UlianaDahnovska@users.noreply.github.com>
2019-12-15 18:17:03 +0200

    fix

--- DahnovskaUO/recursion.py
+++
@@ -1,8 +0,0 @@
-def recursion(dataset):
-    for id in dataset.keys():
-        street = dataset[id]['street']
-        city = dataset[id]['city']
-        price = dataset[id]['price']
-        country_code = dataset[id]['country_code']
-        if price > 15 and price < 100 and country_code == 'US':
-            print('ID: '+ id +'\nStreet: ' + street + '\nCity: ' + city + '\nPrice: ' + price + '\n')
