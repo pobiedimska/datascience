ad4da315277374d1993021b53c6f9ed6927ed9db
Sonya <55147091+pobiedimska@users.noreply.github.com>
2019-12-15 13:10:07 +0200

    updated validator

--- pobiedimskasi/validator/lib.py
+++ pobiedimskasi/validator/lib.py
@@ -1,23 +1,28 @@
 import re
 
+
 def validator(prompt_input, prompt_error, pattern):
     text = input(prompt_input).replace(" ", "")
     while not bool(pattern.match(text)):
         text = input(prompt_error).replace(" ", "")
     return text
 
+
 def provider_id_validator():
     return validator('Please input Provider ID: ', 'Sorry, provider ID must consist of 6 numbers. Try again: ',
                      re.compile("\s*\d{6}\s*"))
 
+
 def state_validator():
     return validator('Please input state: ', 'Please input state abbreviation. Try again: ',
-                     re.compile("\s*\w{2}\s*"))
+                     re.compile("\s*\w{2}\s*")).upper()
+
 
 def percent_of_beneficiaries_with_depression_validator():
-    value = validator('Please input percent of beneficiaries with depression: ', 'Percent must be a number between 0 and 100. Try again: ',
-                     re.compile("\s*\d{1, 3}\s*"))
-    while value<0 or value>100:
+    value = validator('Please input percent of beneficiaries with depression: ',
+                      'Percent must be a number between 0 and 100. Try again: ',
+                      re.compile("\s*\d{1, 3}\s*"))
+    while float(value) < 0 or float(value) > 100:
         print('Percent can not be higher than 100 or lower than 0.')
         value = validator('Please input percent of beneficiaries with depression: ',
                           'Percent must be a number between 0 and 100. Try again: ',
@@ -29,7 +34,7 @@ def percent_of_beneficiaries_with_hyperlipidemia_validator():
     value = validator('Please input percent of beneficiaries with hyperlipidemia: ',
                       'Percent must be a number between 0 and 100. Try again: ',
                       re.compile("\s*\d{1, 3}\s*"))
-    while value < 0 or value > 100:
+    while float(value) < 0 or float(value) > 100:
         print('Percent can not be higher than 100 or lower than 0.')
         value = validator('Please input percent of beneficiaries with hyperlipidemia: ',
                           'Percent must be a number between 0 and 100. Try again: ',
