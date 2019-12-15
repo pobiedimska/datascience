3e119091fbb060b29e20ea940b5f583f6eda9a8e
igortereshchenko <tereshchenko.igor@gmail.com>
2019-12-15 16:49:26 +0200

    my programs

--- PoliatskaAA/recursion.py
+++ PoliatskaAA/recursion.py
@@ -3,7 +3,7 @@ def recursion(data,count = [0],get_last_key=False,last_key=None,req=0):
         last_key = list(data.keys())[-1]
         get_last_key = True
     for key in data.keys():
-        if data[key] == 'red':
+        if 'red' in data[key]:
             count[0]+=1
         elif isinstance(data[key],dict):
             recursion(data[key],count,get_last_key,last_key,req+1)
