ec75f8db033a63d5512e09324b436f45ac44002e
eugene <sazoneugene04@gmail.com>
2019-12-15 18:11:44 +0200

    dataset_structure

--- Sazonenko/dataset_structure.py
+++ Sazonenko/dataset_structure.py
@@ -10,3 +10,16 @@ dataset = {
                                'wire_htap': 'No',
                                'borocode': 1}
 }
+
+
+# def get_all_good_status (datase):
+#     counter = 1
+#     key = list(dataset)[0]
+#     if dataset[key]['status'] == 'good':
+#         counter += 1
+#     dataset.pop(key)
+#     while len(dataset)>0:
+#         get_all_good_status(dataset)
+#     return counter
+#
+# print(get_all_good_status(dataset))
