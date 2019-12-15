56f3afc5556ad95420cbda7e063684d5b36b5525
UlianaDahnovska <55241559+UlianaDahnovska@users.noreply.github.com>
2019-12-15 18:01:02 +0200

    recursion

--- DahnovskaUO/dataset_structure.py
+++ DahnovskaUO/dataset_structure.py
@@ -4,20 +4,20 @@ dataset = {
         'country_code':'US',
         'city':'Seattle',
         'street':'Gilman Dr W, Seattle, WA 98119, United States',
-        'price':'$85.00'
+        'price':'85.00'
         },
     '953595':
         {
         'country_code':'US',
         'city':'Seattle',
         'street':'7th Avenue West, Seattle, WA 98119, United States',
-        'price':'$150.00'
+        'price':'150.00'
         },
     '3308979':
         {
         'country_code':'US',
         'city':'Seattle',
         'street':'West Lee Street, Seattle, WA 98119, United States',
-        'price':'$975.00'
+        'price':'975.00'
         }
     }
