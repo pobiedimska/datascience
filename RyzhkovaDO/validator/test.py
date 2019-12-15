12e2746f6286e2ec7846a35048182014ce76d887
dasha-ryzhkova <ryzhkovadasha2002@.gmail.com>
2019-12-15 19:57:29 +0200

    validator

--- RyzhkovaDO/validator/test.py
+++ RyzhkovaDO/validator/test.py
@@ -8,4 +8,3 @@ vert_wall = vert_wall_validator(re_vert_wall, "Введіть vert_wall (Yes/No)
 zipcode = zipcode_validator(re_zipcode, "Введіть zipcode (5 цифр): ")
 borocode = borocode_validator(re_borocode, "Введіть borocode (Одна цифра з проміжку [1-5]): ")
 
-#objectid_validator, vert_wall_validator, zipcode_validator, borocode_validator
