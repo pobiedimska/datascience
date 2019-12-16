9d1c2f428ae7a8e195cc1f9cd567ee2f24b62a62
aSaltykov <55931826+aSaltykov@users.noreply.github.com>
2019-12-16 10:29:18 +0200

    Update dataset_structure.py
    Changed the file dataset_structure

--- Saltykov Andrey/dataset_structure.py
+++ Saltykov Andrey/dataset_structure.py
@@ -1,21 +1,14 @@
-dataset = {
-
-    1: {
-        'room_type': 'Private room',
-        'price': 85,
-        'weekly_price': '$852.00',
-        'beds': 1
-    },
-    2: {
-        'room_type': 'Entire home/apt',
-        'price': 8,
-        'weekly_price': '$815.00',
-        'beds': 2
-    },
-    3: {
-        'room_type': 'Entire home/apt',
-        'price': 5,
-        'weekly_price': '$835.00',
-        'beds': 3
-    }
-}
+dataset = [
+    {'room_type': 'Private room',
+     'price': 85,
+     'weekly_price': '$852.00',
+     'beds': 1},
+    {'room_type': 'Entire home/apt',
+     'price': 8,
+     'weekly_price': '$815.00',
+     'beds': 2},
+    {'room_type': 'Entire home/apt',
+     'price': 5,
+     'weekly_price': '$835.00',
+     'beds': 3},
+]
