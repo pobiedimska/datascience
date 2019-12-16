8ec9379e43a15f8928d09a555388b3850da74f78
taraspyndykivskiy <taraspyndykivskiy@gmail.com>
2019-12-16 23:39:47 +0200

    validators, description updated

--- pyndykivskiytr/recursion.py
+++ pyndykivskiytr/recursion.py
@@ -11,9 +11,9 @@ def info(dataset, iterator=0):
 
 		if(dataset[keys[iterator]]["average_age"]>30) and (dataset[keys[iterator]]["total_lupa_episodes"]>2):
 			print(keys[iterator] + '\t\t   ' + dataset[keys[iterator]]["city"] + ' '*(30-len(dataset[keys[iterator]]["city"])) + str(dataset[keys[iterator]]["total_lupa_episodes"]))
-
+				
 		info(dataset, iterator+1)
 
 print("Table info that satisfies conditions : ")
-print("\nProvider id\t\tCity\t\tTotal lupa episodes")
+print("\nProvider ID\t\tCity\t\tTotal lupa episodes")
 info(dataset)
