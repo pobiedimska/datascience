ffb45f6305bcb330ca2fdae488054353f3249dbf
Andrey <andronm93@gmail.com>
2019-12-16 21:34:49 +0200

    Changed the name of the package, added the file dataset_insert_manual and added the validator with the test file

---
+++ SaltykovAn/dataset_insert_manual.py
@@ -0,0 +1,23 @@
+dataset = [
+    {'room_type': 'Private room',
+     'price': 85,
+     'weekly_price': '$852.00',
+     'beds': 1},
+    {'room_type': 'Entire home/apt',
+     'price': 8,
+     'weekly_price': '$815.00',
+     'beds': 2},
+    {'room_type': 'Entire home/apt',
+     'price': 5,
+     'weekly_price': '$835.00',
+     'beds': 3},
+]
+
+dataset += [{
+          'room_type': 'Private room',
+          'price' : 30,
+          'weekly_price' : '$52.00',
+          'beds' : 0
+        }]
+
+print(dataset)
