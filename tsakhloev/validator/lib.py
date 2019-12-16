268f43bc036d7901545c4de081ffd002a8d6ac15
Edrick Edorty <edik24032002@gmail.com>
2019-12-16 19:53:48 +0200

    Зробив таск 5: написанi валiдатори та тести до них.

--- tsakhloev/validator/lib.py
+++ tsakhloev/validator/lib.py
@@ -1,13 +1,20 @@
-import re
+from re import compile
+
+int_pattern = compile( r'^\d+$' )
+
+money_pattern = compile( r'^\$\d+.\d\d$' )
+
+calendar_patterns = [ r'yesterday', r'tomorrow', r'^\d*| (weeks|months|days) ago$', r'^a (day|week|month) ago', r'an hour ago' ]
+calendar_patterns = [ compile( pattern ) for pattern in calendar_patterns ]
 
 def host_id_validator( text ):
-	pass	
+    return True if int_pattern.match( text ) else False
 
 def cleaning_fee_validator( text ):
-	pass
+    return True if text == '' or money_pattern.match( text ) else False
 
 def calendar_updated_validator( text ):
-	pass
+    return True if text == '' or any( [ pattern.match( text ) for pattern in calendar_patterns ] ) else False
 
 def extra_people_validator( text ):
-	pass
+    return True if text == '' or money_pattern.match( text ) else False
