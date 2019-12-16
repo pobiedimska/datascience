a9dbf9408d580051cff8c79f72e2db1bda5ad461
Nic <ignatenkolya@gmail.com>
2019-12-16 13:15:55 +0300

    validators + dataset

---
+++ IghnatenkoMO/dataset_insert_manual.py
@@ -0,0 +1,8 @@
+from IghnatenkoMO.dataset_structure import dataset
+dataset.update({
+    "Avpe_Oob1cnluZO-bnWr":{
+        "count":"None",
+        "dimension":"None",
+        "flavors":"None"}
+})
+print(dataset)
---
+++ IghnatenkoMO/dataset_structure.py
@@ -0,0 +1,30 @@
+dataset = {
+    "AVpe_0ob1cnluZ0-bnWr":{
+        "count":["null"],
+        "dimension": {
+            "height":"6.0 in x",
+            "length":"6.0 in x",
+            "width":"1.0 in"},
+        "flavors": ["null"]},
+    "AVpe__eOilAPnD_xSt-H":{
+        "count":["null"],
+        "dimension":["null"],
+        "flavors":["null"]},
+    "AVpe_54gilAPnD_xSr-B":{
+        "count":["null"],
+        "dimension": {
+            "height":"6.0 in x",
+            "length":"6.0 in x",
+            "width":"1.0 in"},
+        "flavors":["null"]}
+}
+
+
+    
+
+
+
+
+
+
+
