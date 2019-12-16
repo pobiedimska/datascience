665ebdce4300b20adf9f992a5c6130baa8863b3e
Глєб Щегельський <55458299+hleppps@users.noreply.github.com>
2019-12-16 22:14:43 +0200

    Доробив таск 5.

--- shchehelskyiho/validator/test.py
+++ shchehelskyiho/validator/test.py
@@ -1,17 +1,20 @@
 from lib import *
 
-if zipcode_validator('98119'):
-    print('Дані правильні')
-else: print('Дані неправильні')
+def output(attribute, ds_correct, ds_incorrect, ds_of_functions):
+    print('Атрибут:', attribute)
+    print('Дані правильні:', ds_correct)
+    print('Дані неправильні:', ds_incorrect, '\n')
 
-if street_validator('8th Avenue West, Seattle, WA 98119, United States'):
-    print('Дані правильні')
-else: print('Дані неправильні')
+ds_correct = {'zipcode': '98119', 'street': '8th Avenue West, Seattle, WA 98119, United States', 'space': 'I hope this space will make my guests feel like they (EMAIL HIDDEN)fortable, in a great location and peaceful for an ideal getaway.', 'host_id': '1452570'}
+ds_incorrect = {'zipcode': 'abc12', 'street': 'Вулиця Політехнічна, 22', 'space': 'It\'s an amazing flat.\n                  The park is near.', 'host_id': 'My host_id is: 1452570'}
+ds_of_attributes = ds_correct.keys()
 
-if space_validator('I hope this space will make my guests feel like they (EMAIL HIDDEN)fortable, in a great location and peaceful for an ideal getaway.'):
-    print('Дані правильні')
-else: print('Дані неправильні')
+for i in ds_of_attributes:
 
-if host_id_validator('1452570'):
-    print('Дані правильні')
-else: print('Дані неправильні')
+    ds_of_functions = {
+        '0': zipcode_validator(i),
+        '1': street_validator(i),
+        '2': space_validator(i),
+        '3': host_id_validator(i)}
+
+    output(i, ds_correct[i], ds_incorrect[i], ds_of_functions1[i])
