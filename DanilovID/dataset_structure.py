bcfe828a0915e57021e2009896c3f58c7f7a02f3
IvanDanilovKM-94 <ivan_danilov@ukr.net>
2019-12-16 19:33:38 +0200

    fixed dataset_structure

--- DanilovID/dataset_structure.py
+++ DanilovID/dataset_structure.py
@@ -1,21 +1,22 @@
 dataset = {
-    "956883":
-        {
-            "room_type":"Entire home/apt",
-            "bedrooms":"1",
-            "country":"United States"
+    956883:
+          {
+         "room_type":"Entire home/apt",
+         "bedrooms":1,
+         "country":"United States"
 
-        },
-    "2166277":
-        {
-            "room_type":"Private room",
-            "bedrooms":"1",
-            "country":"United States"
-        },
-    "25906186":
-        {
-            "room_type":"Entire home/apt",
-            "bedrooms":"4",
-            "country":"United States"
-        }
-     }
+         },
+
+    2166277:
+          {
+         "room_type":"Private room",
+         "bedrooms":1,
+         "country":"United States"
+         },
+    25906186:
+          {
+         "room_type":"Entire home/apt",
+         "bedrooms":4,
+         "country":"United States"
+         }
+             }
