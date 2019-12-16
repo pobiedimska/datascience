231fe99aedc7a3e9af3c1cb56414fe032ecc9ebe
Alina Germanchuk <al5663312@gmail.com>
2019-12-16 15:23:25 +0300

    Змінено ім'я

---
+++ GermanchukAV/dataset_structure.py
@@ -0,0 +1,17 @@
+dataset = {
+    "241032":{
+        "country_code" :"US",
+        "bed_type":"Real Bed",
+        "price":85.00
+    },
+    "5177328":{
+        "country_code" :"US",
+        "bed_type":"Real Bed",
+        "price":150.00
+    },
+    "16708587": {
+        "country_code": "US",
+        "bed_type": "Real Bed",
+        "price": 975.00
+    }}
+
