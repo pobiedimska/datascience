0b65a0e0739931d1a199ecc58f0967317fe903c8
Lada678 <55762041+Lada678@users.noreply.github.com>
2019-12-16 21:26:47 +0200

    Update dataset_structure.py

--- kondrashovavo/dataset_structure.py
+++ kondrashovavo/dataset_structure.py
@@ -1,20 +1,23 @@
 dataset = {
-  "747203":{
-     "state":"TX"
-     "asian_pacific_islander_beneficiaries":"0"
-     "hispanic_beneficiaries":" "
-       }
-   "747758":{
-     "state":"TX"
-     "asian_pacific_islander_beneficiaries":"0"
-     "hispanic_beneficiaries":"0"
-       }
-    "109795":{
-       "state":"FL"
-       "asian_pacific_islander_beneficiaries":"0"
-       "hispanic_beneficiaries":"31"
-       }
-     }
+     '747203':
+          {
+         'state':'TX',
+         'asian_pacific_islander_beneficiaries':'0',
+         'hispanic_beneficiaries':' '
+          }
+     '747758':
+         {
+        'state':'TX',
+        'asian_pacific_islander_beneficiaries':'0',
+        'hispanic_beneficiaries':'0'
+          }
+     '109795':
+          {
+        'state':'FL',
+        'asian_pacific_islander_beneficiaries':'0',
+        'hispanic_beneficiaries':'31'
+           }
+            }
           
 
 
