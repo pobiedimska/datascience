a0ec3cef0952455ae55d58d72b7d51ffd822053f
Sophia Rodina <sophia.rodina.ib@gmail.com>
2019-12-16 16:17:00 +0200

    dataset_insert_manual

--- Lanovenko/validator/test.py
+++ Lanovenko/validator/test.py
@@ -1,6 +1,27 @@
 from validator import lab
 
-host_id = lab.host_id_validator("host ID: ")
-beds = lab.beds_validator("beds: ")
-number_reviews = lab.number_reviews_validator("number of reviews: ")
-country = lab.country_validator("country: ")
+host_id = input("Host ID: ")
+while not lab.host_id_validator(host_id):
+    print("Expression is invalid! Try again")
+    host_id = input("Host ID: ")
+else:
+    host_id = int(host_id)
+
+beds = input("Number of beds: ")
+while not lab.beds_validator(beds):
+    print("Expression is invalid! Try again")
+    beds = input("Beds: ")
+else:
+    beds = int(beds)
+
+number_reviews = input("Number of reviews: ")
+while not lab.number_reviews_validator(number_reviews):
+    print("Expression is invalid! Try again")
+    number_reviews = input("Numbers of reviews: ")
+else:
+    host_id = int(number_reviews)
+
+country = input("Country: ")
+while not lab.country_validator(country):
+    print("Expression is invalid! Try again")
+    country = input("Country: ")
