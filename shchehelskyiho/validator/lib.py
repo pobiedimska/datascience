ef157f39365abad6fd0b4a74d67510724a2d4c08
Глєб Щегельський <55458299+hleppps@users.noreply.github.com>
2019-12-17 18:10:47 +0200

    Виправив помилки в таск 5.

--- shchehelskyiho/validator/lib.py
+++ shchehelskyiho/validator/lib.py
@@ -4,7 +4,7 @@ def zipcode_validator(zipcode):
     return True if re.match(r'^\d+$', zipcode) else False
 
 def street_validator(street):
-    return True if re.match(r'^[\w*\s*\,]+United States$', street) else False
+    return True if re.match(r'^[\w\,\-\/\\\s]+$', street) else False
 
 def space_validator(space):
     return True if re.match(r'^.*$', space) else False
--- shchehelskyiho/validator/test.py
+++ shchehelskyiho/validator/test.py
@@ -1,20 +1,20 @@
 from lib import *
 
 def output(attribute, ds_correct, ds_incorrect, ds_of_functions):
-    print('Атрибут:', attribute)
-    print('Дані правильні:', ds_correct)
-    print('Дані неправильні:', ds_incorrect, '\n')
+    print('Атрибут:', attribute, '\nДані '+ str(ds_of_functions) + ':', ds_correct) if ds_of_functions else print('Дані '+ str(ds_of_functions) + ':', ds_incorrect, '\n')
 
 ds_correct = {'zipcode': '98119', 'street': '8th Avenue West, Seattle, WA 98119, United States', 'space': 'I hope this space will make my guests feel like they (EMAIL HIDDEN)fortable, in a great location and peaceful for an ideal getaway.', 'host_id': '1452570'}
-ds_incorrect = {'zipcode': 'abc12', 'street': 'Вулиця Політехнічна, 22', 'space': 'It\'s an amazing flat.\n                  The park is near.', 'host_id': 'My host_id is: 1452570'}
+ds_incorrect = {'zipcode': 'abc12', 'street': 'Вулиця Політехнічна, 22###', 'space': 'It\'s an amazing flat.\n            The park is near.', 'host_id': 'My host_id is 1452570'}
 ds_of_attributes = ds_correct.keys()
+ds = [ds_correct, ds_incorrect]
 
-for i in ds_of_attributes:
+for index, elem in enumerate(ds_of_attributes):
+    for j in range(2):
 
-    ds_of_functions = {
-        '0': zipcode_validator(i),
-        '1': street_validator(i),
-        '2': space_validator(i),
-        '3': host_id_validator(i)}
+        ds_of_functions = {
+            0: zipcode_validator(ds[(j)][elem]),
+            1: street_validator(ds[(j)][elem]),
+            2: space_validator(ds[j][elem]),
+            3: host_id_validator(ds[j][elem])}
 
-    output(i, ds_correct[i], ds_incorrect[i], ds_of_functions1[i])
+        output(elem, ds_correct[elem], ds_incorrect[elem], ds_of_functions[index])
