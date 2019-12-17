d2c37b2bcc5a07342af44ffee07626de27427e58
OnlyOneTry <olegimgur@gmail.com>
2019-12-17 23:46:42 +0200

    Commit

--- Oleh Sydorov/dataset_structure.py
+++
@@ -1,7 +0,0 @@
-dataset_example = {
-                    'LONDON PLANETREE':{
-                                        'Spc_latin': 'PLATANUS ACERIFOLIA'
-                                        ,'Status': 'Good'
-                                        ,'Pit type': 'Lawn'
-                                                            }
-                    }
---
+++ sydorovos/validator/test.py
@@ -0,0 +1,9 @@
+import sydorovos.validator.lib as test
+print(test.zip_city_validator('New York'),
+test.zip_city_validator('2Bsd!@'),
+test.horz_blck_validator('Yes'),
+test.horz_blck_validator('Da'),
+test.sen_year_validator('2020'),
+test.sen_year_validator('24r56'),
+test.st_assem_validator('1'),
+test.st_assem_validator('2'))
