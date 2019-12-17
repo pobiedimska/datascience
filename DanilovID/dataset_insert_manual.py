1cad4eccd90f91f58bf9dbb775a9ee4fec9061fb
IvanDanilovKM-94 <ivan_danilov@ukr.net>
2019-12-17 10:47:00 +0200

    made task 4

---
+++ DanilovID/dataset_insert_manual.py
@@ -0,0 +1,8 @@
+from dataset_structure import dataset
+dataset.update({
+	1243056:
+	      {
+         "room_type":"Shared room",
+         "bedrooms":1,
+         "country":"United States"
+         }})
