f353f6a5980b1debcd93da5559f819cc124dd5ea
Святослав <swyat.usatov@gmail.com>
2019-12-17 22:36:42 +0200

    fix validator

--- usatovsv/validator/lib.py
+++ usatovsv/validator/lib.py
@@ -1,22 +1,22 @@
 import re
 def cb_num_validator(cb_num):
-    while bool(re.match(r"^\d+$",cb_num)):
+    if bool(re.match(r"^\d+$",cb_num)):
         return True
     else:
         return False
 
 def zipcode_validator(zipcode):
-    while bool(re.match(r"^\d+$", zipcode)):
+    if bool(re.match(r"^\d+$", zipcode)):
         return True
     else:
         return False
 def wire_2nd_validator(wire_2nd):
-    while bool(re.match(r"(False)|(True)", wire_2nd)):
+    if bool(re.match(r"(False)|(True)", wire_2nd)):
         return True
     else:
         return False
 def spc_latin_validator(spc_latin):
-    while bool(re.match(r"[A-Z]+(\s)[A-Z]+", spc_latin)):
+    if bool(re.match(r"[A-Z]+(\s)[A-Z]+", spc_latin)):
         return True
     else:
         return False
