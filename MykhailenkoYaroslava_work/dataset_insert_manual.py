2f674d953ca7c37aecfd81c06599d222f1f1fe11
YaroslavaMykhailenko <55324111+YaroslavaMykhailenko@users.noreply.github.com>
2019-12-17 23:45:25 +0300

    Update dataset_insert_manual.py

--- MykhailenkoYaroslava_work/dataset_insert_manual.py
+++ MykhailenkoYaroslava_work/dataset_insert_manual.py
@@ -32,10 +32,8 @@ for city_info_my in city_info:
 input_key = input("Виберіть ключ,до якого будемо додавати рядок:")
 a = 0
 for key in city_info:
-    # print(key)
     if input_key != key:
         print("")
-        # break
     else:
         a =1
 if a==1:
@@ -45,14 +43,6 @@ if a==1:
             my_dataset = {input_key: input_data}
             city_info[input_key][input_text] = input_data
             print(city_info)
-    # if input_key =="horz_plant":
-    #         my_dataset = {input_key: input_data}
-    #         city_info[input_key][input_text] = input_data
-    #         print(city_info)
-    # if input_key =="zip_city":
-    #         my_dataset = {input_key: input_data}
-    #         city_info[input_key][input_text] = input_data
-    #         print(city_info)
 else:
     print("Такого ключа не існує!")
 
