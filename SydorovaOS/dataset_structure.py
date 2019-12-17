f2922a36e56c098b24de1cbb3545ba008232dcaa
Olexandra Sydorova <aleksandrasidorova075@gmail.com>
2019-12-17 13:47:20 +0200

    add dataset,description

---
+++ SydorovaOS/dataset_structure.py
@@ -0,0 +1,20 @@
+dataset = {1 :{
+                        'status': {'old'}
+                        ,'inf_shoes': {'No'}
+                        ,'trunk_dmg':{'None'}
+                      }
+           ,2:{
+                         'status': {'old'}
+                        ,'inf_shoes': {'No'}
+                        ,'trunk_dmg':{'None'}
+                       }
+           ,3:{
+                         'status': {'new'}
+                        ,'inf_shoes': {'No'}
+                        ,'trunk_dmg':{'None'}
+                       }
+
+    }
+
+
+
