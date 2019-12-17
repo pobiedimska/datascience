fe6cdfe578c0fab08fa4b52bc2c073b216aca5ed
BohdanC <55244228+BohdanC@users.noreply.github.com>
2019-12-17 19:32:29 +0200

    Update recursion.py

--- ChukhBV/recursion.py
+++ ChukhBV/recursion.py
@@ -1,3 +1,4 @@
+from dataset_structure import dataset
 def recursion(dataset,n=0):
     while len(dataset.keys())!=n:
         id=list(dataset.keys())[n]
@@ -9,25 +10,4 @@ def recursion(dataset,n=0):
             print("\nID:"+str(id) + "\nMarket:"+str(market)+"\nCountry:" + str(country)+"\nState:"+str(state))
         return recursion(dataset,n+1)
 
-dataset={
-    "7421966":{
-        "neighbourhood":"Queen Anne",
-        "state":"WA",
-        "market":"Seattle",
-        "country":"United States"
-    },
-    "1266459":{
-        "neighbourhood":"Ballard",
-        "state":"WA",
-        "market":"Seattle",
-        "country":"United States"
-    },
-    "6202214":{
-        "neighbourhood":"Wallingford",
-        "state":"WA",
-        "market":"Seattle",
-        "country":"United States"
-    }
-
-}
 recursion(dataset)
