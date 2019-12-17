5c5a2bfe57885ead47ea3102abd876cfea3c2e09
Drobyazko <philipjava1111@gmail.com>
2019-12-17 19:48:30 +0200

    dataset_structure_common

---
+++ DrobyazkoOV/intergration/dataset_common_insert.py
@@ -0,0 +1,4 @@
+from DrobyazkoOV.intergration.dataset_structure_common import dataset
+
+dataset["sdfksd233"] = 17
+print(dataset)
---
+++ DrobyazkoOV/intergration/dataset_structure_common.py
@@ -0,0 +1,39 @@
+
+dataset = {
+        "sdfksd233": {
+                        "name": "Boots",
+                        "prices.offer": {46, 567, 453},
+                        "size": {41, 42, 43, 44},
+                        "count": 15,
+                        "key": {
+                                "handmade",
+                                "blue4",
+                                "comfy"
+                                },
+                        "brand": "Shoes4youth1"
+                        },
+        "fffkf3223": {
+                        "name": "Hills",
+                        "prices.offer": {46, 563, 153},
+                        "size": {41, 42, 43, 44},
+                        "count": 17,
+                        "key": {
+                                "red",
+                                "shoe",
+                                "comf"
+                                },
+                        "brand": "H&M"
+                        },
+        "erfrer323": {
+                        "name": "Shoes",
+                        "prices.offer": {46, 567, 453},
+                        "size": {41, 42, 43, 44},
+                        "count": 17,
+                        "key": {
+                                "black&white",
+                                "for_you",
+                                "comfortable"
+                                },
+                        "brand": "H&M"
+                        }
+            }
