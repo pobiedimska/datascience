e4de39dd1ddf81d2216189bc89853b97257166ca
marim <marimat2014@gmail.com>
2019-12-17 23:01:30 +0100

    recursion

--- Matvienko/dataset_structure.py
+++ Matvienko/dataset_structure.py
@@ -1,20 +1,16 @@
-dataset={
-    "2005":{
-                "horz_plant": "No" ,
-                 "vert_wall":"No" ,
-                "sidw_raise":"No"
-            },
-
-    "2005": {
-            "horz_plant":"No",
-            "vert_wall":"No",
-            "sidw_raise":"No"
-            },
-
-    "2005": {
-                "horz_plant":"No",
-                "vert_wall":"No",
-                "sidw_raise":"No"
-                }
-
-}
+dataset = {
+    1: {"cen_year": 2005,
+        "horz_plant": "No",
+        "vert_wall": "No",
+        "sidw_raise": "No"
+        },
+    2: {"cen_year": 2005,
+        "horz_plant": "No",
+        "vert_wall": "No",
+        "sidw_raise": "No"
+        },
+    3: {"cen_year": 2005,
+        "horz_plant": "No",
+        "vert_wall": "Yes",
+        "sidw_raise": "No"
+        }}
