56c25b1e483bb6453eaad43a41b6fa630f03ec32
KateandEva <pavlovskakate2002@gmail.com>
2019-12-18 15:52:47 +0200

    bugs fixed

--- Lanovenko/validator/test.py
+++ Lanovenko/validator/test.py
@@ -1,27 +1,27 @@
 from validator import lib
 
 host_id = input("Host ID: ")
-while not lab.host_id_validator(host_id):
+while not lib.host_id_validator(host_id):
     print("Expression is invalid! Try again")
     host_id = input("Host ID: ")
 else:
     host_id = int(host_id)
 
 beds = input("Number of beds: ")
-while not lab.beds_validator(beds):
+while not lib.beds_validator(beds):
     print("Expression is invalid! Try again")
     beds = input("Beds: ")
 else:
     beds = int(beds)
 
 number_reviews = input("Number of reviews: ")
-while not lab.number_reviews_validator(number_reviews):
+while not lib.number_reviews_validator(number_reviews):
     print("Expression is invalid! Try again")
     number_reviews = input("Numbers of reviews: ")
 else:
     host_id = int(number_reviews)
 
 country = input("Country: ")
-while not lab.country_validator(country):
+while not lib.country_validator(country):
     print("Expression is invalid! Try again")
     country = input("Country: ")
