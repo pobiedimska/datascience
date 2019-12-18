4026b7c0b9e44815b985c6048bd0ee080b57e47a
unknown <obodasya@gmail.com>
2019-12-18 22:30:08 +0200

    recurtion.validator

---
+++ lashtabegav/dataset_insert_manual.py
@@ -0,0 +1,47 @@
+dataset = 
+{
+
+    "956883": {
+
+        "number_of_reviews": 207,
+
+        "review_scores_value": 30,
+
+        "country": "United States"
+
+    },
+
+    "5177328": {
+
+        "number_of_reviews": 43,
+
+        "review_scores_value": 10,
+
+        "country": "United States"
+
+    },
+
+    "16708587": {
+
+        "number_of_reviews": 9,
+
+        "review_scores_value": 5,
+
+        "country": "United States"
+
+    }
+
+}
+
+
+dataset["956884"] = {
+
+    "number_of_reviews": 207,
+
+    "review_scores_value": 30,
+
+    "country": "United States"
+}
+
+
+print(dataset)
