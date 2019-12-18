5e32453dd3cc476a6ca29babbd7d2ea752e560b3
DELL <kardanformula@gmail.com>
2019-12-18 20:51:00 +0200

    dataset_common_insert

--- Fedorchenkory/Integration/dataset_structure_common.py
+++ Fedorchenkory/Integration/dataset_structure_common.py
@@ -14,7 +14,7 @@ dataset = {
                         'brand': 'NIKE'},
      'AVpe__e0il45eD':{ "count": 500,
                         "name": "Shoses",
-                        "reviews": 500
+                        "reviews": 500,
                         'keys': {'basketballshoes': 677898905645, 'thundermodel': 677898056405},
                         'manufacturer': 'Sports Manufacture Company.Limited',
                         'brand': 'PUMA'}
