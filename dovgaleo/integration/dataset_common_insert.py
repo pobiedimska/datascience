3fbdfba8364bd88b8f212da1c512868127509e2e
pinkLama <55158011+pinkLama@users.noreply.github.com>
2019-12-19 22:25:56 +0200

    integration insert with dict

--- dovgaleo/integration/dataset_common_insert.py
+++ dovgaleo/integration/dataset_common_insert.py
@@ -2,33 +2,33 @@ from dovgaleo.integration.dataset_structure_common import dataset
 from dovgaleo.integration import validator
 
 
+key_validators = {
+        'provider_id': validator.provider_id_validator,
+        'city': validator.city_validator,
+        'zip_code': validator.zip_code_validator,
+        'total_episodes_non_lupa': validator.total_episodes_non_lupa_validator,
+        'percent_of_beneficiaries_with_ra_oa': validator.percent_of_beneficiaries_with_ra_oa_validator,
+        'percent_of_beneficiaries_with_stroke': validator.percent_of_beneficiaries_with_stroke_validator
+}
+
+
 def insert_common_data(dataset):
     if not isinstance(dataset, dict):
         print('Your dataset is not valid')
         return -1
 
-    keys = ['provider_id', 'city', 'zip_code', 'total_episodes_non_lupa',
-            'percent_of_beneficiaries_with_ra_oa', 'percent_of_beneficiaries_with_stroke']
+    provider_id, value = 0, 0
+    for key, validator in key_validators.items():
+        data_is_valid = False
+        while not data_is_valid:
+            value = input(f'{key} = ')
+            data_is_valid = validator(value)
 
-    provider_id = 0
-    for i in range(6):
-        while True:
-            value, check = input(f'{keys[i]} = '), 0
-
-            if keys[i] == keys[0]: check = validator.provider_id_validator(value)
-            if keys[i] == keys[1]: check = validator.city_validator(value)
-            if keys[i] == keys[2]: check = validator.zip_code_validator(value)
-            if keys[i] == keys[3]: check = validator.total_episodes_non_lupa_validator(value)
-            if keys[i] == keys[4]: check = validator.percent_of_beneficiaries_with_ra_oa_validator(value)
-            if keys[i] == keys[5]: check = validator.percent_of_beneficiaries_with_stroke_validator(value)
-
-            if check is True: break
-
-        if i == 0:
+        if provider_id == 0:
             provider_id = value
-            dataset[provider_id] = {}
+            dataset[value] = {}
         else:
-            dataset[provider_id][keys[i]] = value
+            dataset[provider_id][key] = value
 
 
 insert_common_data(dataset)
