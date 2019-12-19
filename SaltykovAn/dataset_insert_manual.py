ee071cdf3dc4ffb64e661cc930fb31fca7c48df9
Andrey <andronm93@gmail.com>
2019-12-19 13:18:43 +0200

    Changed the file dataset_insert_manual

--- SaltykovAn/dataset_insert_manual.py
+++ SaltykovAn/dataset_insert_manual.py
@@ -1,10 +1,8 @@
 from dataset_structure import dataset
 
-dataset += [{
-          'room_type': 'Private room',
-          'price' : 30,
-          'weekly_price' : '$52.00',
-          'beds' : 0
-        }]
+
+def add_el(dataset, added_room_type, added_price , added_weekly_price,added_beds):
+    dataset += [{ "room_type":added_room_type, "price":added_price, "weekly_price":added_weekly_price, "beds":added_beds}]
+add_el(dataset,'Private room',35,'$816.00',0)
 
 print(dataset)
