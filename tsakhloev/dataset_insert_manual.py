427ab5542c98e2c5105c763814fb9a8c22e2e614
Edrick Edorty <edik24032002@gmail.com>
2019-12-19 19:06:22 +0200

    Збільшив ефективність функції recursion. Додав можливість обирати dataset у dataset_insert_manual.

--- tsakhloev/dataset_insert_manual.py
+++ tsakhloev/dataset_insert_manual.py
@@ -1,7 +1,7 @@
 from dataset_structure import dataset
 from validator.lib import *
 
-def dataset_insert_manual( host_id, cleaning_fee, calendar_updated, extra_people ):
+def dataset_insert_manual( dataset, host_id, cleaning_fee, calendar_updated, extra_people ):
 
     validators = [ host_id_validator(host_id), cleaning_fee_validator(cleaning_fee), calendar_updated_validator(calendar_updated), extra_people_validator(extra_people) ]
 
