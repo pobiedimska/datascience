55181e775dc6d7574a22056beeb816ee3dda4671
Edrick Edorty <edik24032002@gmail.com>
2019-12-19 19:47:22 +0200

    Виконані завдання 6 та 7: зроблена інтеграція зі студентом Щегельский Г.О.. Переписана функція dataset_common_insert для більшої гнучкості у використанні. Виправлена помилка у валідаторі 'грошей'.

---
+++ tsakhloev/integration/dataset_structure_common.py
@@ -0,0 +1,36 @@
+dataset = {
+
+    4016632: {
+
+        'cleaning-fee': '$45.00',
+        'calendar-updated': 'yesterday',
+        'extra-people': '$5.00',
+        'space': 'Kitchen has hot water tap and sodastream Excellent water pressure Original art throughout the house Dogs under 30 lbs welcome',
+        'street': 'West Olympic Place, Seattle, US 98119, United States',
+        'zipcode': 98119
+
+    },
+
+    9851441: {
+
+        'cleaning-fee': None,
+        'calendar-updated': '6 months ago',
+        'extra-people': '$110.00',
+        'space': None,
+        'street': '2nd Avenue West, Seattle, WE 98119, United States',
+        'zipcode': 98119
+
+    },
+
+    239585: {
+
+    	'cleaning-fee': '$15.00',
+        'calendar-updated': 'a week ago',
+        'extra-people': '$15.00',
+        'space': 'I hope this space will make my guests feel like they (EMAIL HIDDEN)fortable, in a great location and peaceful for an ideal getaway.',
+        'street': 'Northwest 56th Street, Seattle, WA 98107, United States  ',
+        'zipcode': 98107
+
+    }
+
+}
