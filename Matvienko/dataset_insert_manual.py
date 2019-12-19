b44a5bfc68a1791d6df6c012b541ce39c2d4a7b6
marim <marimat2014@gmail.com>
2019-12-19 19:56:35 +0100

    changes

--- Matvienko/dataset_insert_manual.py
+++ Matvienko/dataset_insert_manual.py
@@ -1,14 +1,15 @@
 from dataset_structure import dataset
 
-def insert(dataset, cen_year, horz_plant, vert_wall, sidw_raise):
-    dataset[len(dataset)+1] = {"cen_year": cen_year
-        , "horz_plant": horz_plant
-        , "vert_wall": vert_wall
-        , "sidw_raise": sidw_raise
-                                 }
+def insert(dataset,year, horz_plant, vert_wall, sidw_raise):
+    dataset.update({year:{
+        "horz_plant":horz_plant,
+        "vert_wall":vert_wall,
+        "sidw_raise":sidw_raise
+    }})
+    return dataset
 
 
-insert(dataset, 2006, "No", "No", "Yes")
+insert(dataset, 2015, "No", "No", "Yes")
 
 print(dataset)
 
--- Matvienko/dataset_structure.py
+++ Matvienko/dataset_structure.py
@@ -1,15 +1,15 @@
 dataset = {
-    1: {"cen_year": 2005,
+    "2005": {
         "horz_plant": "No",
         "vert_wall": "No",
         "sidw_raise": "No"
         },
-    2: {"cen_year": 2005,
+    "2006": {
         "horz_plant": "No",
         "vert_wall": "No",
         "sidw_raise": "No"
         },
-    3: {"cen_year": 2005,
+    "2007": {
         "horz_plant": "No",
         "vert_wall": "Yes",
         "sidw_raise": "No"
