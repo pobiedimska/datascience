955423574f5000a7d67cf02b007796d308117ae5
Глєб Щегельський <55458299+hleppps@users.noreply.github.com>
2019-12-19 19:05:34 +0200

    Виконав таск 6.

---
+++ shchehelskyiho/integration/dataset_common_insert.py
@@ -0,0 +1,4 @@
+from tereshchenkoio.intergration.dataset_structure_common import dataset
+
+dataset['Boban'] = 17
+print(dataset)
---
+++ shchehelskyiho/integration/dataset_structure_common.py
@@ -0,0 +1,27 @@
+dataset = {
+
+    14100483:{
+        'space': 'Kitchen has hot water tap and sodastream Excellent water pressure Original art throughout the house Dogs under 30 lbs welcome',
+        'street': 'West Olympic Place, Seattle, US 98119, United States',
+        'zipcode': 98119,
+        'cleaning-fee': '$45.00',
+        'calendar-updated': 'yesterday',
+        'extra-people': '$5.00'
+    },
+    21401856:{
+        'space': None,
+        'street': '2nd Avenue West, Seattle, WE 98119, United States',
+        'zipcode': 98119,
+        'cleaning-fee': None,
+        'calendar-updated': '6 months ago',
+        'extra-people': '$110.00'
+    },
+    7466082:{
+        'space': 'I hope this space will make my guests feel like they (EMAIL HIDDEN)fortable, in a great location and peaceful for an ideal getaway.',
+        'street': 'Northwest 56th Street, Seattle, WA 98107, United States  ',
+        'zipcode': 98107,
+        'cleaning-fee': '$15.00',
+        'calendar-updated': 'a week ago',
+        'extra-people': '$15.00'
+    },
+}
