ad6b7165a6f3f33fedf9cbab0f0485df69a12ac3
MaximShcherbachenko <55317211+MaximShcherbachenko@users.noreply.github.com>
2019-12-19 23:23:21 +0200

    Add values as parameters

--- shcherbachenkoma/dataset_insert_manual.py
+++ shcherbachenkoma/dataset_insert_manual.py
@@ -1,3 +1,3 @@
-def add_info(patient, dataset):
-    dataset[patient] = {"agency_name": "UNITED AMERICA HOME HEALTH SERVICES, INC.", "average_hcc_score": 2.24,
-                        "percent_of_beneficiaries_with_asthma": 19}
+def add_info(unique_number_of_patient, agency_name, average_hcc_score, percent_of_beneficiaries_with_asthma, dataset):
+    dataset[unique_number_of_patient] = {"agency_name": agency_name, "average_hcc_score": average_hcc_score,
+                                         "percent_of_beneficiaries_with_asthma": percent_of_beneficiaries_with_asthma}
