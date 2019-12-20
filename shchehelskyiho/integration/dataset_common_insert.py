91c1087b79f7c32e7dab2478dd855dc3a3dfd889
hleppps <ddddffffgggg36@gmail.com>
2019-12-20 14:04:17 +0200

    Виконав тиски 6-7. Інтеграція з Корнієнко Т.

--- shchehelskyiho/integration/dataset_common_insert.py
+++ shchehelskyiho/integration/dataset_common_insert.py
@@ -1,4 +1,33 @@
-from tereshchenkoio.intergration.dataset_structure_common import dataset
+from dataset_structure_common import dataset
+import sys
 
-dataset['Boban'] = 17
-print(dataset)
+sys.path.append('../../KornienkoT/validator')
+from lib import *
+
+sys.path.append('../')
+from validator.lib import *
+
+def dataset_common_insert(dataset, host_id, new_dataset):
+    attributes = new_dataset.keys()
+    funcs = {'space' : space_validator,
+            'street' : street_validator,
+            'zipcode' : zipcode_validator,
+            'price' : price_validator,
+            'minimum_nights' : minimum_nights_validator,
+            'weekly_price' : weekly_price_validator}
+    for index in attributes:
+        if not funcs[index](new_dataset[index]):
+            return
+    dataset[host_id] = new_dataset
+
+host_id = 32713558
+new_dataset = {
+    'space' : 'This home encompasses the character that is so sought after on Queen Anne. It is a large Craftsman with a beautiful and private garden. The front porch, gourmet kitchen, master suite, basement tv/ga...',
+    'street' : '1st Avenue West, Seattle, WA 98119, United States',
+    'zipcode' : '98119',
+    'price' : '450',
+    'minimum_nights' : '6',
+    'weekly_price' : '800'
+    }
+
+dataset_common_insert(dataset, host_id, new_dataset)
--- shchehelskyiho/integration/dataset_structure_common.py
+++ shchehelskyiho/integration/dataset_structure_common.py
@@ -4,24 +4,24 @@ dataset = {
         'space': 'Kitchen has hot water tap and sodastream Excellent water pressure Original art throughout the house Dogs under 30 lbs welcome',
         'street': 'West Olympic Place, Seattle, US 98119, United States',
         'zipcode': 98119,
-        'cleaning-fee': '$45.00',
-        'calendar-updated': 'yesterday',
-        'extra-people': '$5.00'
+        'price' : '300',
+        'minimum_nights' : '4',
+        'weekly_price' : '600'
     },
     21401856:{
         'space': None,
         'street': '2nd Avenue West, Seattle, WE 98119, United States',
         'zipcode': 98119,
-        'cleaning-fee': None,
-        'calendar-updated': '6 months ago',
-        'extra-people': '$110.00'
+        'price' : '450',
+        'minimum_nights' : '6',
+        'weekly_price' : '800'
     },
     7466082:{
         'space': 'I hope this space will make my guests feel like they (EMAIL HIDDEN)fortable, in a great location and peaceful for an ideal getaway.',
         'street': 'Northwest 56th Street, Seattle, WA 98107, United States  ',
         'zipcode': 98107,
-        'cleaning-fee': '$15.00',
-        'calendar-updated': 'a week ago',
-        'extra-people': '$15.00'
+        'price' : '700',
+        'minimum_nights' : '7',
+        'weekly_price' : '400'
     },
 }
