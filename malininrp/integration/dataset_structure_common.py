31d1f06712cec78a3e10aaabc8121cd468828b86
Malinin Rostislav <rarka333@gmail.com>
2019-12-21 18:20:44 +0200

    Added dataset_structure_common and dataset_common_insert

---
+++ malininrp/integration/dataset_structure_common.py
@@ -0,0 +1,33 @@
+dataset = {
+	
+	"Stylish Queen Anne Apartment" :{
+        "host_name" : "Maija" ,
+        "street" : "Gilman Dr W, Seattle, WA 98119, United States",
+        "host_response_time" : 10,
+        'room_type': 'Private room',
+		'price' : 85,
+		'weekly_price' : '$852.00',
+		'beds' : 1
+    },
+
+    "Bright & Airy Queen Anne Apartment":{
+        "host_name" : "Andrea",
+        "street" : "7th Avenue West, Seattle, WA 98119, United States",
+        "host_response_time" : 10,
+        'room_type': 'Private room',
+		'price' : 85,
+		'weekly_price' : '$852.00',
+		'beds' : 1
+    },
+
+    "New Modern House-Amazing water view" :{
+        "host_name" : "Jill",
+        "street" : "West Lee Street, Seattle, WA 98119, United States",
+        "host_response_time" : 10,
+        'room_type': 'Private room',
+		'price' : 85,
+		'weekly_price' : '$852.00',
+		'beds' : 1
+    }
+
+}
