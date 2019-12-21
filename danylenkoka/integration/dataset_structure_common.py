a93be94c5e3b49e314cd6b345a3c466b0a8c3b27
kdanylenko <qqqwsq@gmail.com>
2019-12-22 00:03:14 +0300

    dataset_structure_common

---
+++ danylenkoka/integration/dataset_structure_common.py
@@ -0,0 +1,25 @@
+integrated_dataset = {
+
+    'New-York' : {
+        'vert_wall': True,
+        'st_accem':0,  # Атрибут Василия
+        'st_senate':0, # Атрибут Василия
+        'cen_year': 2018,
+    },
+
+
+    'West-Side': {
+        'vert_wall': False,
+        'wire_2nd': 1,
+        'boroname': 'Queens', # Атрибут Василия
+        'cen_year': 2011,
+    },
+
+
+    'East-Side': {
+        'wire_2nd': 222,
+        'cen_year': 1998,
+        'st_senate':0, # Атрибут Василия
+    }
+
+}
