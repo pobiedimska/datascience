de2797d7c10cd55f409776573cc1baa4a8d5d81e
ilyabondarkm93 <55483219+ilyabondarkm93@users.noreply.github.com>
2019-12-21 13:09:33 +0200

    added dataset_structure_common

---
+++ bondarilya/integration/dataset_structure_common.py
@@ -0,0 +1,27 @@
+dataset = {
+    "245787" : {
+        "state" : "TX",
+        "other_unknown_beneficiaries" : "0",
+        "percent_of_beneficiaries_with_alzheimers" : "29",
+        'avarage_age': "55",
+        'male_beneficiares' : "73",
+        'female_beneficaries' : "53"
+    },
+    "534675" : {
+        "state" : "FL",
+        "other_unknown_beneficiaries" : "0",
+        "percent_of_beneficiaries_with_alzheimers" : "7",
+        'avarage_age': "57",
+        'male_beneficiares' : "44",
+        'female_beneficaries' : "75"
+    },
+    "325723": {
+        "state" : "IL",
+        "other_unknown_beneficiaries" : "0",
+        "percent_of_beneficiaries_with_alzheimers" : "16",
+        'avarage_age': "53",
+        'male_beneficiares' : "103",
+        'female_beneficaries' : "62"
+
+    }
+}
