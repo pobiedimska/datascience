cea1808cfe4b4f903f4e252db3bc85dae5d5b567
Katekisiv <katerinakisiv@gmail.com>
2019-12-21 14:46:31 -0700

    fix validators

--- kisivks/validator/lib.py
+++ kisivks/validator/lib.py
@@ -2,32 +2,20 @@ import re
 
 
 def provider_id_validator(data):
-    match = re.fullmatch(r'[0-9]+', data)
-    if match:
-        return True
-    else:
-        return False
+    match = re.fullmatch(r'[0-9]{6}', data)
+    return match is not None
 
 
 def zip_code_validator(data):
     match = re.fullmatch(r'[0-9]+', data)
-    if match:
-        return True
-    else:
-        return False
+    return match is not None
 
 
 def percent_of_beneficiaries_with_ra_oa_validator(data):
-    match = re.fullmatch(r'^100$|^[1-9][0-9]$', data)
-    if match:
-        return True
-    else:
-        return False
+    match = re.fullmatch(r'^100$|^[1-9]?[0-9]$', data)
+    return match is not None
 
 
 def percent_of_beneficiaries_with_stroke_validator(data):
-    match = re.fullmatch(r'^100$|^[1-9][0-9]$', data)
-    if match:
-        return True
-    else:
-        return False
+    match = re.fullmatch(r'^100$|^[1-9]?[0-9]$', data)
+    return match is not None
